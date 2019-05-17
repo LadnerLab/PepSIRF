c578ccca9b1058ae1aa9ebdaf50e7a55a80602ca
Bob Costas <finkzane@gmail.com>
2019-05-17 13:55:51 -0700

    added input_r1 and input_r2 and library_fname to options class

--- include/options.h
+++ include/options.h
@@ -1,10 +1,17 @@
 #ifndef OPTIONS_HH_INCLUDED
 #define OPTIONS_HH_INCLUDED
+#include <string>
 
 class options
 {
 public:
     options();
+
+    std::string input_r1_fname;
+    std::string input_r2_fname;
+    std::string library_fname;
+
+    int num_threads;
 };
 
 #endif //OPTIONS_HH_INCLUDED
