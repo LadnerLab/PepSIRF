dae5dc59e8434c95d6f15fe09e207b801858830e
Bob Costas <finkzane@gmail.com>
2019-05-17 13:18:11 -0700

    moved creation of opts to safe code

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,23 @@
 
 int main( int argc, char **argv )
 {
+    // create objects to be used
     options_parser parser;
     options opts;
 
     try
         {
+            // create our option parser for parsing command-line options
             parser = options_parser();
+            // parse the arguments, any incorrect arguments will raise an error
+            opts = parser.parse( argc, &argv );
         }
     catch( std::exception& e )
         {
+            // command-line errors will be handled here
             std::cerr << "Error: " << e.what() << std::endl;
 
             return EXIT_FAILURE;
         }
 
-    opts = parser.parse( argc, &argv );
 }
