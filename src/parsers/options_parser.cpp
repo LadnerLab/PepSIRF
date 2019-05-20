16d726a4bdc432b9f5876f4b26f1758f700b106e
Bob Costas <finkzane@gmail.com>
2019-05-20 14:16:29 -0700

    added default value to num_threads argument

--- src/parsers/options_parser.cpp
+++ src/parsers/options_parser.cpp
@@ -10,14 +10,14 @@ void options_parser::parse( int argc, char ***argv, options& opts )
 
     po::options_description desc( "PepSIRF: Peptide-based Serological Immune Response Framework" );
     desc.add_options()
-        ( "help", "Produce help message" )
-        ( "input_r1", po::value<std::string>()->required(), "Input forward reads fastq file to parse.")
-        ( "input_r2", po::value<std::string>()->required(), "Input reverse reads fastq file to parse.")
-        ( "library,l", po::value<std::string>()->required(), "Designed library containing amino acid peptides. "
+        ( "help,h", "Produce help message" )
+        ( "input_r1", po::value<std::string>( &opts.input_r1_fname )->required(), "Input forward reads fastq file to parse.")
+        ( "input_r2", po::value<std::string>( &opts.input_r2_fname )->required(), "Input reverse reads fastq file to parse.")
+        ( "library,l", po::value<std::string>( &opts.library_fname )->required(), "Designed library containing amino acid peptides. "
                                                              "Library should be in fasta format and should contain "
                                                              "sequences that were used to design input_r1 and input_r2."
         )
-        ( "num_threads,t", po::value<int>(), "Number of threads to use for analyses." );
+        ( "num_threads,t", po::value<int>( &opts.num_threads )->default_value( opts.DEFAULT_NUM_THREADS ), "Number of threads to use for analyses." );
 
     po::store( po::parse_command_line( argc, argv_loc, desc ), vm );
 
