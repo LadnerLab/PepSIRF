70688ed38ddefa11b609ae777a79ea574d3dede1
Bob Costas <finkzane@gmail.com>
2019-05-20 13:54:09 -0700

    added doxygen documentation

--- include/options.h
+++ include/options.h
@@ -2,16 +2,20 @@
 #define OPTIONS_HH_INCLUDED
 #include <string>
 
+/*! Data class to contain and handle 
+ * arguments that will be passed in from the 
+ * command-line.
+*/
 class options
 {
 public:
-    options();
+    options(); //!< Default constructor.
 
-    std::string input_r1_fname;
-    std::string input_r2_fname;
-    std::string library_fname;
+    std::string input_r1_fname; //!< Filename for forward reads, can be a .zip archive or a regular fastq file.
+    std::string input_r2_fname; //!< Filename for reverse reads, can be a .zip archive or regular fastq file.
+    std::string library_fname; //!< Filename containing a FASTA file containing a library of amino acid peptide sequences.
 
-    int num_threads;
+    int num_threads; //!< The number of threads to use for computation.
 };
 
 #endif //OPTIONS_HH_INCLUDED
--- include/options_parser.h
+++ include/options_parser.h
@@ -8,11 +8,27 @@
 
 #include "options.h"
 
-
+/*!
+ * Basic class for parsing options from the commandline. Makes use of 
+ * boost::program_options. 
+ */
 class options_parser
 {
 public:
-    options_parser();
+    /**
+     * Default constructor.
+     **/
+    options_parser(); 
+
+    /**
+     * Parse command-line arguments, store parsed options in the 
+     * members of the opts item.
+     * @param argc The number of arguments passed from the command-line.
+     * @param argv Pointer to '**argv' obtained from the main function.
+     * @param opts Reference to opts object, values will be populated by 
+     *             the arguments from the commandline.
+     * @throws error upon required argument not supplied.
+     **/
     void parse( int argc, char ***argv, options& opts );
 };
 
