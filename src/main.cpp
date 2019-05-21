b1f3dee36723f58b860c95c4ce025e5ebde9ad95
Bob Costas <finkzane@gmail.com>
2019-05-21 15:17:50 -0700

    added creation of creation of module parser

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,26 @@
 #include <exception>
 #include <stdlib.h>
 
-#include "options_parser_demux.h"
-#include "options_demux.h"
+#include "options_parser_factory.h"
+#include "options_factory.h"
+#include "options_parser.h"
+#include "options.h"
 
 int main( int argc, char **argv )
 {
     // create objects to be used
-    options_parser_demux parser;
-    options_demux opts;
+    options_parser *parser;
+    options *opts;
+    options_parser_factory parser_fact;
 
     try
         {
             // create our option parser for parsing command-line options
-            parser = options_parser_demux();
+            parser = parser_fact.create( argc, &argv );
+
+            // opts = opts_fact.create( argc, &argv );
             // parse the arguments, any incorrect arguments will raise an error
-            parser.parse( argc, &argv, opts );
+            parser->parse( argc, &argv, *opts );
         }
     catch( std::exception& e )
         {
@@ -28,6 +33,6 @@ int main( int argc, char **argv )
         }
 
     // run PepSIRF with options parsed from command-line
-
+    delete parser;
     return EXIT_SUCCESS;
 }
