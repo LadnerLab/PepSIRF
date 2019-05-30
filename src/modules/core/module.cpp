70a1bece9fdc32b0f3fd156120f048fff1e9ffde
Bob Costas <finkzane@gmail.com>
2019-05-30 14:23:14 -0700

    added dummy 'run' method

--- src/modules/core/module.cpp
+++ src/modules/core/module.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
 #include "module.h"
 
 module::module() = default;
 module::~module() = default;
+
+void module::run( options *opts )
+{
+    if( opts->num_threads == 0 )
+        {
+            std::cout << "Zero threads!\n";
+        }
+}
