d8e62bcfea16cbc5df849d14a5a7d99df426ec2f
Bob Costas <finkzane@gmail.com>
2019-05-30 13:50:51 -0700

    added documentation

--- include/modules/core/module_factory.h
+++ include/modules/core/module_factory.h
@@ -2,11 +2,27 @@
 #define MODULE_FACTORY_HH_INCLUDED
 #include "module.h"
 
+/**
+ * Class for creating modules based on a string identifier passed as a 
+ * parameter to the create method.
+ **/
 class module_factory
 {
  public:
+    /**
+     * Default constructor
+     **/ 
     module_factory();
-    virtual module *create( const char *module_name );
+
+    /**
+     * Virtual method to create a moduel given the name of the module.
+     * @param module_name String containing the name of module to create.
+     * @pre module_name is a valid module name
+     * @note The returned module will be created with 'new', and must be deleted.
+     * @returns pointer to a module that has been dynamically allocated, or null if 
+     *          an invalid module name is passed
+     **/
+    module *create( const char *module_name );
 
 
 };
