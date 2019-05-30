1f5363ad693274139cb6a83ee552b77330991792
Bob Costas <finkzane@gmail.com>
2019-05-30 13:47:03 -0700

    creation of module class

---
+++ include/modules/core/module.h
@@ -0,0 +1,23 @@
+#ifndef MODULE_HH_INCLUDED
+#define MODULE_HH_INCLUDED
+
+#include "options.h"
+
+class module
+{
+
+ public:
+    /**
+     * Default constructor.
+     **/
+    module(); 
+
+    /**
+     * Run a module.
+     * @param opts Pointer to an options object, each module will have 
+     *        its own type of options. 
+     **/
+    virtual void run( options *opts );
+
+};
+#endif 
---
+++ src/modules/core/module.cpp
@@ -0,0 +1,3 @@
+#include "module.h"
+
+module::module() = default;
