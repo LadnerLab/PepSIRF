bf74470861ccb829884e0e0b4ad6f537eab5ec47
Bob Costas <finkzane@gmail.com>
2019-05-31 12:22:50 -0700

    added comments, while is now dependent on std::getline.good()

--- src/modules/parsers/fasta_parser.cpp
+++ src/modules/parsers/fasta_parser.cpp
@@ -12,9 +12,7 @@ std::vector<sequence> fasta_parser::parse( std::string filename )
     std::string line;
     std::ifstream input_file( filename, std::ios_base::in );
 
-    bool is_seq = false;
-
-    while( std::getline( input_file, line ) )
+    while( std::getline( input_file, line ).good() )
         {
             // skip any empty lines
             if( line.length() > 0 )
@@ -24,9 +22,11 @@ std::vector<sequence> fasta_parser::parse( std::string filename )
                         {
                             if( seq.name.compare( "" ) )
                                 {
+                                    // if the name has been changed, add the last-created sequence
                                     seq_vector.push_back( seq );
                                 }
 
+                            // create a new sequence, set its name to the line
                             seq = sequence();
                             seq.name = line;
                         }
