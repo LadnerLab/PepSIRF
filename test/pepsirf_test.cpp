16f150e9ecdd4ba94053de34266b33a7d6c0f2a8
Bob Costas <finkzane@gmail.com>
2019-05-31 12:18:25 -0700

    added testing for fasta_parser

--- test/pepsirf_test.cpp
+++ test/pepsirf_test.cpp
@@ -2,10 +2,13 @@
 #include "catch.hpp"
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include "sequence.h"
 #include "fasta_parser.h"
 #include "fastq_parser.h"
 
+static std::string fasta_ex = ">Example sequence 1\nDJFKDLFJSF\nDJFKDJFDJKFJKDF\n\nJDSKFLJFDKSFLJ\n>Example Sequence 2\n\n\nDJFKLSFJDKLSFJKSLFJSKDLFJDKSLFJKLDKDKDK\n\n\n";
 TEST_CASE( "Sequence", "[sequence]" )
 {
     std::string n = "Sequence name";
@@ -22,4 +25,41 @@ TEST_CASE( "Sequence", "[sequence]" )
     REQUIRE( seq.seq.compare( s2 ) == 0 );
     REQUIRE( seq.count( 'A' ) == 1 );
     REQUIRE( seq.count( 'Z' ) == 0 );
+
+    s2 = "AAA";
+    REQUIRE( seq.seq.compare( s2 ) != 0 );
+}
+
+TEST_CASE( "Parse Fasta", "[fasta_parser]" )
+{
+    fasta_parser fp;
+    std::vector<sequence> vec;
+    vec = fp.parse( "../test/test.fasta" );
+
+    REQUIRE( vec.size() == 110 );
+
+    unsigned int index = 0;
+    for( index = 0; index < vec.size(); ++index )
+        {
+            REQUIRE( vec[ index ].seq.compare( "" ) );
+            REQUIRE( vec[ index ].name.compare( "" ) );
+        }
+
+    std::ofstream stream;
+    stream.open( "out.fasta" );
+    stream << fasta_ex;
+    stream.close();
+
+    vec = fp.parse( "out.fasta" );
+    REQUIRE( vec.size() == 2 );
+    REQUIRE( !vec[ 0 ].name.compare( ">Example sequence 1" ) );
+    REQUIRE( !vec[ 0 ].seq.compare( "DJFKDLFJSFDJFKDJFDJKFJKDFJDSKFLJFDKSFLJ" ) );
+    REQUIRE( !vec[ 1 ].name.compare( ">Example Sequence 2" ) );
+    REQUIRE( !vec[ 1 ].seq.compare( "DJFKLSFJDKLSFJKSLFJSKDLFJDKSLFJKLDKDKDK" ) );
+    REQUIRE( vec[ 1 ].seq.compare( vec[ 0 ].seq ) );
+    REQUIRE( vec[ 0 ].seq.compare( vec[ 1 ].seq ) );
+
+
+    remove( "out.fasta" );
+
 }
