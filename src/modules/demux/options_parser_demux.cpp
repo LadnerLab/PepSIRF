26c6b3cb7efbe7431cbcfa810b50233b2c4fca30
Bob Costas <finkzane@gmail.com>
2019-06-03 12:21:37 -0700

    added read_per_loop arg

--- src/modules/demux/options_parser_demux.cpp
+++ src/modules/demux/options_parser_demux.cpp
@@ -12,11 +12,11 @@ void options_parser_demux::parse( int argc, char ***argv, options *opts )
     desc.add_options()
         ( "help,h", "Produce help message" )
         ( "input_r1", po::value<std::string>( &opts_demux->input_r1_fname )->required(), "Input forward reads fastq file to parse.")
-        ( "input_r2", po::value<std::string>( &opts_demux->input_r2_fname )->required(), "Input reverse reads fastq file to parse.")
-        ( "library,l", po::value<std::string>( &opts_demux->library_fname )->required(), "Designed library containing amino acid peptides. "
+        ( "library,l", po::value<std::string>( &opts_demux->library_fname )->required(), "Designed library containing nucleic acid peptides. "
                                                              "Library should be in fasta format and should contain "
-                                                             "sequences that were used to design input_r1 and input_r2."
+                                                             "sequences that were used to design input_r1."
           )
+        ( "read_per_loop,r", po::value<long int>( &opts_demux->read_per_loop )->default_value( opts_demux->DEFAULT_READ_PER_LOOP, "description" ) )
         ( "num_threads,t", po::value<int>( &opts_demux->num_threads )->default_value( opts_demux->DEFAULT_NUM_THREADS ), "Number of threads to use for analyses." );
 
     po::store( po::command_line_parser( argc, *argv ).options( desc ).allow_unregistered().run(), vm);
