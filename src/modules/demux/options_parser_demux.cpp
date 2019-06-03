441e3611e637cb7a3d4dffa3cf93ece7ba8f5151
Bob Costas <finkzane@gmail.com>
2019-06-03 12:23:23 -0700

    added read_per_loop arg description

--- src/modules/demux/options_parser_demux.cpp
+++ src/modules/demux/options_parser_demux.cpp
@@ -15,8 +15,11 @@ void options_parser_demux::parse( int argc, char ***argv, options *opts )
         ( "library,l", po::value<std::string>( &opts_demux->library_fname )->required(), "Designed library containing nucleic acid peptides. "
                                                              "Library should be in fasta format and should contain "
                                                              "sequences that were used to design input_r1."
-          )
-        ( "read_per_loop,r", po::value<long int>( &opts_demux->read_per_loop )->default_value( opts_demux->DEFAULT_READ_PER_LOOP, "description" ) )
+        )
+        ( "read_per_loop,r", po::value<long int>( &opts_demux->read_per_loop )->default_value( opts_demux->DEFAULT_READ_PER_LOOP, "The number of fastq "
+          "records read a time. A higher value will result in more memory usage by the program, but will also result in fewer disk accesses, "
+          "increasing performance of the program." )
+        )
         ( "num_threads,t", po::value<int>( &opts_demux->num_threads )->default_value( opts_demux->DEFAULT_NUM_THREADS ), "Number of threads to use for analyses." );
 
     po::store( po::command_line_parser( argc, *argv ).options( desc ).allow_unregistered().run(), vm);
