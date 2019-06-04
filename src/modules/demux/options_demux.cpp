d4b31902e02d40c0911878aeae021486254e3f53
Bob Costas <finkzane@gmail.com>
2019-06-04 14:55:24 -0700

    added output of samplelist args, formatted output

--- src/modules/demux/options_demux.cpp
+++ src/modules/demux/options_demux.cpp
@@ -5,10 +5,12 @@ std::string options_demux::get_arguments()
 {
     std::ostringstream str_stream;
 
-    str_stream << "--input_r1_fname " << input_r1_fname << "\n" <<
-                  " --library " << library_fname << "\n" <<
-                  " --read_per_loop " << read_per_loop << "\n" <<
-                  " --num_threads " << num_threads << "\n\n";
+    str_stream << "--input_r1_fname    " << input_r1_fname << "\n" <<
+                  " --samplelist_fname " << samplelist_fname << "\n"
+                  " --library          " << library_fname << "\n" <<
+                  " --read_per_loop    " << read_per_loop << "\n" <<
+                  " --num_threads      " << num_threads << "\n" <<
+                  "\n";
 
     return str_stream.str();
 }
