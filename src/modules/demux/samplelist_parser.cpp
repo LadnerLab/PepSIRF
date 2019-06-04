3e2ab98e822b44004282742767ffaeddefaf9a98
Bob Costas <finkzane@gmail.com>
2019-06-04 14:56:07 -0700

    added const methods for hashing

--- src/modules/demux/samplelist_parser.cpp
+++ src/modules/demux/samplelist_parser.cpp
@@ -1,6 +1,24 @@
 #include "samplelist_parser.h"
+#include <iostream>
 
-std::vector<sample> samplelist_parser::parse( std::string filename )
+std::vector<sample> samplelist_parser::parse( const std::string filename )
 {
+    std::ifstream in_stream( filename );
 
+    std::string id1;
+    std::string id2;
+    std::string name;
+
+    int sample_id = 1;
+
+    std::vector<sample> vec;
+
+    while( in_stream >> id1 >> id2 >> name )
+        {
+            sample samp( id1, id2, name, sample_id );
+            vec.push_back( samp );
+            ++sample_id;
+        }
+
+    return vec;
 }
