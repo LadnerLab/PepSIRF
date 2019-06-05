d3a34523fc13f2817e60328d6f465355ea5e9c3f
Bob Costas <finkzane@gmail.com>
2019-06-05 13:11:25 -0700

    added input_r2 arg

--- src/modules/demux/options_parser_demux.cpp
+++ src/modules/demux/options_parser_demux.cpp
@@ -11,20 +11,24 @@ void options_parser_demux::parse( int argc, char ***argv, options *opts )
     po::options_description desc( "PepSIRF: Peptide-based Serological Immune Response Framework" );
     desc.add_options()
         ( "help,h", "Produce help message" )
-        ( "input_r1", po::value<std::string>( &opts_demux->input_r1_fname )->required(), "Input forward reads fastq file to parse.")
+        ( "input_r1", po::value<std::string>( &opts_demux->input_r1_fname )->required(), "Input forward reads fastq file to parse.\n")
+        ( "input_r2", po::value<std::string>( &opts_demux->input_r2_fname ), "Input reverse reads fastq file to parse. Note that if this argument is "
+          "not supplied only forward indices will be used to identify samples.\n"
+        )
+        ( "index", po::value<std::string>( &opts_demux->index_fname )->required(), "Name of fasta file containing forward and (if included )reverse index sequences.\n")
         ( "library,l", po::value<std::string>( &opts_demux->library_fname )->required(), "Designed library containing nucleic acid peptides. "
                                                              "Library should be in fasta format and should contain "
-                                                             "sequences that were used to design input_r1."
+                                                             "sequences that were used to design input_r1.\n"
         )
         ( "read_per_loop,r", po::value<long int>( &opts_demux->read_per_loop )->default_value( opts_demux->DEFAULT_READ_PER_LOOP ), "The number of fastq "
           "records read a time. A higher value will result in more memory usage by the program, but will also result in fewer disk accesses, "
-          "increasing performance of the program."
+          "increasing performance of the program.\n"
         )
-        ( "samplelist,s", po::value<std::string>( &opts_demux->samplelist_fname )->required(), "A tab-delimited list of samples, one sample per line. Each line "
-          "must have three tab-delimited entries, where the first entry is the forward id of the sample, the second is the reverse id of the sample, and the "
-          "third is the name of the sample. This file MUST not have any spaces, including in any of the ids or sample names."
+        ( "samplelist,s", po::value<std::string>( &opts_demux->samplelist_fname )->required(), "A tab-delimited list of samples, one sample per line. If the samples are "
+          "already indexed by I2 only the forward index (I1) and the sample name are required. The first item in each tab-delimited line is the forward (I1) index, the second "
+          "(if included) is the reverse (I2) index, and the third is the samplename. \n"
         )
-        ( "num_threads,t", po::value<int>( &opts_demux->num_threads )->default_value( opts_demux->DEFAULT_NUM_THREADS ), "Number of threads to use for analyses." );
+        ( "num_threads,t", po::value<int>( &opts_demux->num_threads )->default_value( opts_demux->DEFAULT_NUM_THREADS ), "Number of threads to use for analyses.\n" );
 
     po::store( po::command_line_parser( argc, *argv ).options( desc ).allow_unregistered().run(), vm);
 
