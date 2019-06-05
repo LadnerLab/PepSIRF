60acae2521cfd503604770e6afab54f22a4988cf
Bob Costas <finkzane@gmail.com>
2019-06-05 13:11:48 -0700

    fixed formatting

--- src/modules/demux/options_demux.cpp
+++ src/modules/demux/options_demux.cpp
@@ -5,8 +5,10 @@ std::string options_demux::get_arguments()
 {
     std::ostringstream str_stream;
 
-    str_stream << "--input_r1_fname    " << input_r1_fname << "\n" <<
-                  " --samplelist_fname " << samplelist_fname << "\n"
+    str_stream << "--input_r1          " << input_r1_fname << "\n" <<
+                  " --input_r2          " << input_r2_fname << "\n" << 
+                  " --index            " << index_fname << "\n" << 
+                  " --samplelist       " << samplelist_fname << "\n"
                   " --library          " << library_fname << "\n" <<
                   " --read_per_loop    " << read_per_loop << "\n" <<
                   " --num_threads      " << num_threads << "\n" <<
