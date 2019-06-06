a6d0cd6b06463c80676f6c7c2df361af47d916dc
Bob Costas <finkzane@gmail.com>
2019-06-06 11:38:42 -0700

    implemented perfect-match demultiplexing and counting

--- include/modules/demux/module_demux.h
+++ include/modules/demux/module_demux.h
@@ -51,12 +51,44 @@ class module_demux : public module
     /**
      * Adds sequences to an unordered map, where the key is the string sequence, and the value is a vector of 
      * size_t counts. Each count will be used to keep track of how many times each sequence is found per sample.
-     * @param input_map unordered_map which will have sequences as keys, and vectors of size_t as values. 
+     * @param input_map parallel_map which will have sequences as keys, and vectors of size_t as values. 
      * @param seqs The sequences that will be added to the unordered_map
      * @param num_samples The number of samples that were processed in the sequencing run.
 
      **/
-    void add_seqs_to_map( parallel_map<sequence, std::vector<std::size_t>>& input_map, std::vector<sequence>& seqs, size_t num_samples );
+    void add_seqs_to_map( parallel_map<sequence, std::vector<std::size_t>*>& input_map, std::vector<sequence>& seqs, size_t num_samples );
+
+
+    /**
+     * Writes output to the outfile_name.
+     * Output is a comma-separated file, one line per sequence 
+     * where each comma-separated entry is the count for a certain 
+     * sample. 
+     * @note A header is written to the file labelling each column.
+     * @param outfile_name Name of file to write to.
+     * @param seq_scores A map that couples sequences with a vector of their scores, where 
+     *        the i'th entry in the seq_scores for sequence j is the number of times that 
+     *        sequence j was found in sample i.
+     * @param samples vector of samples. This vector is used to label each of the samples in the 
+     *        vector of each seq_score. Note that samples[ i ].id must equal j[ i ] for each 
+     *        j = 1, 2, ... j.size(), i.e. The id of a sample must correspond with its entry in 
+     *        the count vector.
+     **/
+    void write_outputs( std::string outfile_name,
+                        parallel_map<sequence, std::vector<std::size_t>*>& seq_scores,
+                        std::vector<sample>& samples
+                      );
+
 
 };
+
+namespace demux
+{
+    void create_index_map( sequential_map<std::string, sample>& map,
+                           std::vector<sequence>& index_seqs,
+                           std::vector<sample>& samplelist
+                         );
+
+}
+
 #endif // MODULE_DEMUX_HH_INCLUDED
--- src/modules/demux/module_demux.cpp
+++ src/modules/demux/module_demux.cpp
@@ -1,5 +1,6 @@
 #include "module_demux.h"
 
+
 module_demux::module_demux() 
 {
     name = "Demux";
@@ -10,8 +11,19 @@ void module_demux::run( options *opts )
     // options from the command line
     options_demux *d_opts = (options_demux*) opts;
 
+    std::string index_str;
+
+    int forward_start  = 12;
+    int forward_length = 12;
+
+    int reverse_start    = 21;
+    int reverse_length   = 23;
+
+    size_t read_index = 0;
+
     struct time_keep::timer total_time;
-    std::unordered_map<sequence, std::vector<std::size_t>> reference_counts;
+    parallel_map<sequence, std::vector<std::size_t>*> reference_counts;
+    sequential_map<std::string, sample> index_map;
 
     omp_set_num_threads( opts->num_threads );
 
@@ -19,6 +31,7 @@ void module_demux::run( options *opts )
 
     // vector to store the .fna sequences that represent a designed library
     std::vector<sequence> library_seqs;
+    std::vector<sequence> index_seqs;
     std::vector<sequence> reads;
 
     reads.reserve( d_opts->read_per_loop );
@@ -27,21 +40,89 @@ void module_demux::run( options *opts )
     fasta_parser fasta_p;
     fastq_parser fastq_p;
 
+    samplelist_parser samplelist_p;
+    std::vector<sample> samplelist = samplelist_p.parse( d_opts->samplelist_fname );
+
     // open the read file so we can iterate over it 
     std::ifstream reads_file( d_opts->input_r1_fname, std::ios_base::in );
+    std::ifstream r2_reads;
+
+    if( d_opts->input_r2_fname.length() > 0 )
+        {
+            r2_reads.open( d_opts->input_r2_fname, std::ios_base::in );
+        }
 
     library_seqs = fasta_p.parse( d_opts->library_fname );
-    add_seqs_to_map( reference_counts, library_seqs, 11 );
+    index_seqs   = fasta_p.parse( d_opts->index_fname );
 
-    // while we still have reads to process
+    demux::create_index_map( index_map, index_seqs, samplelist );
+    sequential_map<std::string, sample> sample_table;
+
+    add_seqs_to_map( reference_counts, library_seqs, samplelist.size() );
+
+    std::string adapter;
+    std::string nuc_seq;
+
+    parallel_map<sequence, std::vector<std::size_t>*>::iterator seq_iter;
+
+    unsigned int seq_start = 43;
+    unsigned int seq_length = 90;
+
+    std::size_t processed_total   = 0;
+    std::size_t processed_success = 0;
+
+    // while we still have reads to process, loop terminates when no fastq records
+    // are read from the file
     while( fastq_p.parse( reads_file, reads, d_opts->read_per_loop  ) )
         {
+
+            if( d_opts->input_r2_fname.length() > 0 )
+                {
+                    // get the index sequences for this set of sequences
+                    fastq_p.parse( r2_reads, index_seqs, d_opts->read_per_loop );
+                }
+
+            #pragma omp parallel for private( seq_iter, nuc_seq, read_index, index_str, adapter ) \
+                shared( seq_start, seq_length, d_opts, reference_counts, library_seqs, index_seqs ) reduction( +:processed_total, processed_success )
+            for( read_index = 0; read_index < reads.size(); ++read_index )
+                {
+                    // get the forward and the reverse indexes from the sequence, grab the id
+                    // for this index. This gives the index of the location at the sequence to increment
+                    adapter = reads[ read_index ].seq.substr( forward_start, forward_length );
+
+                    if( index_map.find( adapter ) != index_map.end() )
+                        {
+                            nuc_seq = reads[ read_index ].seq.substr( seq_start, seq_length );
+                            seq_iter = reference_counts.find( sequence( "", nuc_seq ) );
+
+                            if( seq_iter != reference_counts.end() )
+                                {
+                                    ( seq_iter->second->at( index_map[ adapter ].id ) )++;
+
+                                    ++processed_success;
+                                }
+                        }
+
+                    // record the number of records that are processed
+                    ++processed_total;
+                }
             reads.clear();
         }
 
     total_time.end = omp_get_wtime();
 
-    std::cout << "Total elapsed time (seconds): " << time_keep::get_elapsed( total_time ) << ".\n";
+    if( d_opts->input_r2_fname.length() > 0 )
+        {
+            r2_reads.close();
+        }
+
+    std::cout << "Processed " << processed_total << " records in " << time_keep::get_elapsed( total_time ) << " seconds.\n";
+    std::cout << processed_success << " records were an exact match out of "
+              << processed_total << " (" << ( (long double) processed_success / (long double) processed_total ) * 100
+              << "%) successful.\n";
+
+    write_outputs( "out_test", reference_counts, samplelist );
+
 }
 
 
@@ -50,13 +131,75 @@ std::string module_demux::get_name()
     return name;
 }
 
-void module_demux::add_seqs_to_map( std::unordered_map<sequence, std::vector<std::size_t>>& input_map, std::vector<sequence>& seqs, size_t num_samples )
+void module_demux::add_seqs_to_map( parallel_map<sequence, std::vector<std::size_t>*>& input_map, std::vector<sequence>& seqs, size_t num_samples )
 {
-    std::size_t index = 0;
+    std::size_t index        = 0;
+    std::size_t inner_index  = 0;
+
     input_map.reserve( seqs.size() );
 
+    #pragma omp parallel for private( index, inner_index  ) shared ( seqs, input_map, num_samples )
     for( index = 0; index < seqs.size(); ++index )
         {
-            input_map[ seqs[ index ] ] = std::vector<std::size_t>( num_samples );
+            input_map[ seqs[ index ] ] = new std::vector<std::size_t>( num_samples );
+
+            for( inner_index = 0; inner_index < num_samples; ++inner_index )
+                {
+                    input_map[ seqs[ index ] ]->at( inner_index ) = 0;
+                }
+        }
+}
+
+void module_demux::write_outputs( std::string outfile_name,
+                                  parallel_map<sequence, std::vector<std::size_t>*>& seq_scores,
+                                  std::vector<sample>& samples
+                                )
+{
+    std::ofstream outfile( outfile_name, std::ofstream::out );
+    parallel_map<sequence, std::vector<std::size_t>*>::iterator seq_iter = seq_scores.begin();
+
+
+    std::size_t index        = 0;
+    std::size_t second_index = 0;
+
+    for( index = 0; index < seq_scores.size(); ++index )
+        {
+            const sequence& curr = seq_iter->first;
+            const std::vector<std::size_t> *curr_counts = seq_iter->second;
+
+            outfile << curr.name << ",";
+
+            for( second_index = 0; second_index < samples.size() - 1; ++second_index )
+                {
+                    outfile << curr_counts->at( second_index ) << ',';
+                }
+            outfile << curr_counts->at( samples.size() - 1 ) << "\n";
+
+            ++seq_iter;
+            delete curr_counts;
+        }
+    outfile.close();
+}
+
+void demux::create_index_map( sequential_map<std::string, sample>& map,
+                              std::vector<sequence>& index_seqs,
+                              std::vector<sample>& samplelist
+                            )
+{
+    sequential_map<std::string, sample> sample_table;
+
+    unsigned int index = 0;
+
+    for( index = 0; index < samplelist.size(); ++index )
+        {
+            sample_table[ samplelist[ index ].get_ids() ] = samplelist[ index ];
+        }
+
+    for( index = 0; index < index_seqs.size(); ++index )
+        {
+            if( sample_table.find( index_seqs[ index ].name ) != sample_table.end() )
+                {
+                    map[ index_seqs[ index ].seq ] = sample_table[ index_seqs[ index ].name ];
+                }
         }
 }
