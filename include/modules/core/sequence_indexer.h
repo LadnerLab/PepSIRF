13df579238d00ad12f58d1e0eafd1ee53f4dbd22
Bob Costas <finkzane@gmail.com>
2019-06-10 15:06:43 -0700

    added sequence_indexer class

---
+++ include/modules/core/sequence_indexer.h
@@ -0,0 +1,13 @@
+#ifndef SEQUENCE_INDEXER_HH_INCLUDED
+#define SEQUENCE_INDEXER_HH_INCLUDED
+#include "sequence.h"
+
+class sequence_indexer
+{
+ public:
+    sequence_indexer();
+};
+
+
+
+#endif // SEQUENCE_INDEXER_HH_INCLUDED
---
+++ src/modules/core/sequence_indexer.cpp
@@ -0,0 +1,4 @@
+#include "sequence_indexer.h"
+#include "edlib.h"
+
+sequence_indexer::sequence_indexer() = default;
