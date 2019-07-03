6c6f705605c2fd85d8bb202b735ccfbaccbcc892
Bob Costas <finkzane@gmail.com>
2019-07-03 14:21:03 -0700

    added sequence output when one is found

--- scripts/count_r1_i1.py
+++ scripts/count_r1_i1.py
@@ -20,7 +20,7 @@ def main():
     args = argp.parse_args()
 
     samples_df        = parse_snames( args.mapping )
-    concatemer_sample = df_2_sdict( samples_df )
+    concatemer_sample, idx_seqs = df_2_sdict( samples_df )
 
     sample_names = set( samples_df[ 'Sample' ] )
 
@@ -35,15 +35,14 @@ def main():
     r1_lines = read_n_lines_fasta( r1_file, n )
     i1_lines = read_n_lines_fasta( i1_file, n )
 
-    reverse_d = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N' } 
-    reverse = lambda y: ''.join( [ reverse_d[ i ] for i in reversed( y ) ] )
-
     while r1_lines and i1_lines:
         for r1, i1 in zip( r1_lines, i1_lines ):
-            concat = get_seq( r1, r1_idx ) + reverse( get_seq( i1, i1_idx ) )
+            f_seq = get_seq( r1, r1_idx )
+            r_seq = get_seq( i1, i1_idx )
+            concat = f_seq + r_seq
 
             if concat not in concatemer_sample:
-                concatemer_sample[ concat ] = [ 'No Sample', 0 ]
+                concatemer_sample[ concat ] = [ 'No Sample', 0, idx_seqs.get( f_seq, '' ), idx_seqs.get( r_seq, '' ) ]
             concatemer_sample[ concat ][ 1 ] += 1
 
         r1_lines = read_n_lines_fasta( r1_file, n )
@@ -55,13 +54,19 @@ def main():
 
     spl = lambda x: [ x[ 0: r1_idx[ 1 ] ], x[ r1_idx[ 1 ]:: ] ]
     st  = lambda x: [ str( item ) for item in x ]
-    fun = lambda x: '\t'.join( list( st( spl( x[ 0 ] ) + 
-                                         x[1::]
+    fun = lambda x: '\t'.join( list( st( [
+                                         x[ 3 ],
+                                         spl( x[ 0 ] )[ 0 ], 
+                                         x[ 4 ],
+                                         spl( x[ 0 ] )[ 1 ],
+                                         x[ 1 ],
+                                         x[ 2 ]
+                                          ]
                                        )
                                    )
                              )
 
-    print( 'Forward Read\tReverse Read\tSample\tCount' )
+    print( 'Forward Name\tForward Read\tReverse Name\tReverse Read\tSample\tCount' )
     for key, val in concatemer_sample.items():
         if val[ 1 ] >= args.min_score or val[ 0 ] in sample_names:
             print( fun( [ key ] + val ) )
@@ -99,12 +104,22 @@ def to_tuple( string ):
 
 def df_2_sdict( df ):
     out_dict = {}
+    out_idx  = {}
+
+    reverse_d = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N' } 
+    reverse = lambda y: ''.join( [ reverse_d[ i ] for i in reversed( y ) ] )
 
     for idx, row in df.iterrows():
         if not str( row[ 'Sample' ] ).lower() == 'nan':
-            concat = row[ 'Forward Sequence' ] + row [ 'Reverse Sequence' ]
-            out_dict[ concat ] = [ row[ 'Sample' ], 0 ]
-    return out_dict
+            rev = reverse( row[ 'Reverse Sequence' ] )
+            concat = row[ 'Forward Sequence' ] + rev
+            out_dict[ concat ] = [ row[ 'Sample' ], 0,
+                                   row[ 'Forward Name' ], row[ 'Reverse Name' ]
+                                 ]
+            out_idx[ row[ 'Forward Sequence' ] ] = row[ 'Forward Name' ]
+            out_idx[ row[ 'Reverse Sequence' ] ] = row[ 'Reverse Name' ]
+
+    return out_dict,out_idx
 
 def parse_snames( fname ):
     csv = pd.read_csv( fname )
