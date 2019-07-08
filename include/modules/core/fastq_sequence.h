79c15d2523d1fe78db263e27d0199b4a6a7526b0
Bob Costas <finkzane@gmail.com>
2019-07-08 12:54:17 -0700

    added function and class documentation

--- include/modules/core/fastq_sequence.h
+++ include/modules/core/fastq_sequence.h
@@ -8,43 +8,49 @@
 #include "sequence.h"
 
 
+/**
+ * Class to represent a fastq sequence. 
+ * The only difference between this and the base sequence class 
+ * is the presence of a score string. This string can be used to 
+ * determine the quality of reads.
+ **/
 class fastq_sequence : public sequence
 {
 
  public:
 
+    /**
+     * Initializer that includes the score threshold. 
+     * @param in_name The name of the sequence that 
+     *        has been read from the fastq.
+     * @param in_seq The sequence that has been read from the fastq
+     * @param score_str The score vector of quality for the read.
+     **/
     fastq_sequence( const std::string& in_name,
                     const std::string& in_seq,
                     const std::string& score_str
                   ) :
                   sequence( in_name, in_seq )
         {
-
             scores = score_str;
         }
 
-    fastq_sequence( const std::string& in_name,
-                    const std::string& in_seq,
-                    const std::string& score_str,
-                    int ascii_base
-                  ) :
-                  sequence( in_name, in_seq )
-        {
-
-            base = ascii_base;
-            scores = score_str;
-        }
-
+    /**
+     * Initializer that does NOT the score threshold. 
+     * @param in_name The name of the sequence that 
+     *        has been read from the fastq.
+     * @param in_seq The sequence that has been read from the fastq
+     **/
     fastq_sequence( const std::string& in_name,
                     const std::string& in_seq
                   ) :
-                  sequence( in_name, in_seq )
-        {
-            base = 0;
-        }
+                  sequence( in_name, in_seq ) {}
 
+
+    /**
+     * Scores vector.
+     **/
     std::string scores;
-    int base;
 
 };
 
