399516e9270eca3ea7559aecaba307de2234e914
Bob Costas <finkzane@gmail.com>
2019-07-09 12:06:25 -0700

    added parse method

---
+++ include/modules/deconv/options_parser_deconv.h
@@ -0,0 +1,35 @@
+#ifndef OPTIONS_PARSER_DECONV_HH_INCLUDED
+#define OPTIONS_PARSER_DECONV_HH_INCLUDED
+
+#include "options_parser.h"
+#include "options_deconv.h"
+
+/**
+ * Basic class for parsing options from the commandline. Makes use of 
+ * boost::program_options. Note that this class is used for parsing options 
+ * pertinent to the 'deconv' module.
+ **/
+class options_parser_deconv : public options_parser
+{
+ public:
+
+    /**
+     * Default constructor.
+     **/
+    options_parser_deconv();
+
+    /**
+     * Parse command-line arguments, store parsed options in the 
+     * members of the opts item.
+     * @param argc The number of arguments passed from the command-line.
+     * @param argv Pointer to '**argv' obtained from the main function.
+     * @param opts Reference to opts object, values will be populated by 
+     *             the arguments from the commandline.
+     * @throws error upon required argument not supplied.
+     * @returns boolean true if arguments other than '-h, --help' have 
+     *          been supplied, false otherwise.
+     **/
+    bool parse( int argc, char ***argv, options *opts );
+
+};
+#endif // OPTIONS_PARSER_DECONV_HH_INCLUDED
---
+++ src/modules/deconv/options_parser_deconv.cpp
@@ -0,0 +1,36 @@
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <iterator>
+#include <exception>
+#include <stdexcept>
+
+#include "options_parser_deconv.h"
+
+
+options_parser_deconv::options_parser_deconv() = default;
+
+bool options_parser_deconv::parse( int argc, char ***argv, options *opts )
+{
+    options_deconv *opts_deconv = (options_deconv*) opts;
+    namespace po = boost::program_options;
+    po::variables_map vm;
+
+    po::options_description desc( "PepSIRF: Peptide-based Serological Immune Response Framework species deconvolution module. \n"
+                                );
+    desc.add_options()
+        ( "help,h", "Produce help message" );
+
+    po::store( po::command_line_parser( argc, *argv ).options( desc ).allow_unregistered().run(), vm);
+
+    if( vm.count( "help" ) )
+        {
+            std::cout << desc << std::endl;
+            return false;
+        }
+    else
+        {
+            po::notify( vm );
+            return true;
+        }
+}
+
