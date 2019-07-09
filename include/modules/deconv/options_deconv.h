580074afec90f6d627d46ab6054b597830b793a4
Bob Costas <finkzane@gmail.com>
2019-07-09 11:36:21 -0700

    added get_arguments method, class declaration

---
+++ include/modules/deconv/options_deconv.h
@@ -0,0 +1,17 @@
+#ifndef OPTIONS_DECONV_HH_INCLUDED
+#define OPTIONS_DECONV_HH_INCLUDED
+
+#include <string>
+
+class options_deconv
+{
+ public:
+    
+    /**
+     * Returns the arguments that are stored by 
+     * the options object.
+     **/
+    std::string get_arguments();
+};
+
+#endif // OPTIONS_DECONV_HH_INCLUDED
---
+++ src/modules/deconv/options_deconv.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <sstream>
+
+#include "options_deconv.h"
+
+std::string options_deconv::get_arguments()
+{
+    std::ostringstream str_stream;
+
+    str_stream << ""
+                  "\n";
+
+    return str_stream.str();
+}
