78b1fcb29c71b51304d7a8a8d8ad468d47fedaa7
Bob Costas <finkzane@gmail.com>
2019-07-10 13:46:51 -0700

    added get_score, parse_linked_file

--- include/modules/deconv/module_deconv.h
+++ include/modules/deconv/module_deconv.h
@@ -1,9 +1,12 @@
 #ifndef MODULE_DECONV_HH_INCLUDED 
 #define MODULE_DECONV_HH_INCLUDED 
 #include <string>
+#include <vector>
+#include <algorithm>
 
 #include "module.h"
 #include "options_deconv.h"
+#include "maps.h"
 
 class module_deconv : public module
 {
@@ -11,6 +14,21 @@ class module_deconv : public module
     module_deconv();
     std::string get_name();
     void run( options *opts );
+
+    parallel_map<std::size_t, int>
+        parse_linked_file( std::string fname );
+
+    int get_score( std::size_t size );
+
+};
+
+template <class K>
+struct compare_vec
+{
+    bool operator()( std::vector<K>& first, std::vector<K>& second )
+    {
+        return first.size() < second.size();
+    }
 };
 
 #endif // MODULE_DECONV_HH_INCLUDED 
--- src/modules/deconv/module_deconv.cpp
+++ src/modules/deconv/module_deconv.cpp
@@ -1,5 +1,10 @@
-#include "module_deconv.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include "module_deconv.h"
 
 module_deconv::module_deconv() = default;
 
@@ -10,5 +15,60 @@ std::string module_deconv::get_name()
 
 void module_deconv::run( options *opts )
 {
-    std::cout << "Module deconv!\n";
+    options_deconv *d_opts = ( options_deconv * ) opts;
+
+    auto map = parse_linked_file( d_opts->linked_fname );
+
+}
+
+parallel_map<std::size_t, int>
+module_deconv::parse_linked_file( std::string fname )
+{
+    std::ifstream in_stream( fname );
+
+    parallel_map<std::size_t, int>
+        ret_map;
+    std::string line;
+
+    int lineno = 0;
+
+    while( std::getline( in_stream, line ) )
+        {
+            if( lineno )
+                {
+                    std::vector<std::string> split_line;
+                    boost::trim_right( line );
+                    boost::split( split_line, line,
+                                  boost::is_any_of( "\t" )
+                                );
+
+
+                    std::vector<std::string> comma_delimited;
+                    if( split_line.size() > 1 )
+                        {
+                            boost::split( comma_delimited, split_line[ 1 ],
+                                          boost::is_any_of( "," )
+                                        );
+
+                            for( auto it = comma_delimited.begin(); it != comma_delimited.end(); ++it )
+                                {
+                                    std::size_t id = boost::lexical_cast<std::size_t>( *it );
+
+                                    if( ret_map.find( id ) == ret_map.end() ) 
+                                        {
+                                            ret_map[ id ] = 0;
+                                        }
+                                    ret_map[ id ] += get_score( comma_delimited.size() );
+                                }
+                        }
+                }
+            ++lineno;
+        }
+
+    return ret_map;
+}
+
+int module_deconv::get_score( std::size_t size )
+{
+    return 1;
 }
