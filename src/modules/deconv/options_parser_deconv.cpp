f803a1e828bf27e792a9f2b51d60526fc354999f
Bob Costas <finkzane@gmail.com>
2019-07-18 10:07:38 -0700

    explanations for id_name_map and summation/fractional scoring

--- src/modules/deconv/options_parser_deconv.cpp
+++ src/modules/deconv/options_parser_deconv.cpp
@@ -39,10 +39,23 @@ bool options_parser_deconv::parse( int argc, char ***argv, options *opts )
           "defined by the number of peptides that share a 7mer with that species. For fractional scoring "
           "the score of each species is defined by 1/n for each peptide, where n is the number of species "
           "a peptide shares a 7mer with. In this method of scoring "
-          "peptides with fewer species are worth more.\n" 
+          "peptides with fewer species are worth more. Note that if neither this flag nor --summation_scoring "
+          "are included, integer scoring will be used. In integer scoring each species is scored by the "
+          "number of peptides it shares a kmer with.\n" 
         )
         ( "summation_scoring", po::bool_switch( &opts_deconv->summation_scoring )->default_value( false ),
-          ""
+          "Include this flag (without any arguments) if you want summation scoring to be used instead of "
+          "fractional or integer scoring. For summation scoring, the --linked file passed must be of the "
+          "form created by --create_linkage. This means a file of tab-delimited values, one per line. \n"
+          "Each line is of the form peptide_name TAB id:score,id:score, and so on. Undefined behavior "
+          "will result if input is not in this format. For summation scoring, each species is scored "
+          "based on the number of kmers it shares with each peptide with which it shares a kmer.\n "
+          "For example, assume a line in the --linked file looks like the following: \n"
+          "peptide_1 TAB 123:4,543:8\n"
+          "Both species '123' and '543' will receive a score of 4 and 8 respectively."
+          "Note that if neither this flag nor --summation_scoring "
+          "are included, integer scoring will be used. In integer scoring each species is scored by the "
+          "number of peptides it shares a kmer with.\n" 
         )
         ( "enriched,e", po::value<std::string>( &opts_deconv->enriched_fname ),
           "File containing the names of enriched peptides, one per line. "
@@ -64,9 +77,11 @@ bool options_parser_deconv::parse( int argc, char ***argv, options *opts )
         )
         ( "k_size,k", po::value<std::size_t>( &opts_deconv->k ), "Kmer size to use.\n" )
         ( "id_name_map", po::value<std::string>( &opts_deconv->id_name_map_fname )->default_value( "" ),
-          "Map with tab-delimited lines, where the first item in each line is a species and the second is the name "
-          "of the species.\n"
-          );
+          "File containing mappings from taxonomic id to name. This file should be formatted like the "
+          "file 'taxidlineage.dmp' from NCBI. It is recommended to either use this file or a subset of this file "
+          "that at least contains the species ids of the designed peptides. If included, the output will contain "
+          "a column denoting the name of the species as well as the id.\n"
+        );
 
     po::store( po::command_line_parser( argc, *argv ).options( desc ).allow_unregistered().run(), vm);
 
