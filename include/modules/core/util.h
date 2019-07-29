a96d0638e402ed36f80f14ac821beb0b0802c730
Bob Costas <finkzane@gmail.com>
2019-07-29 10:36:08 -0700

    added simple math opts

--- include/modules/core/util.h
+++ include/modules/core/util.h
@@ -15,6 +15,31 @@ namespace util
             }
 
     }
+
+    template<class T>
+        T divide( T a, T b )
+        {
+            assert( b > 0 );
+            return a / b;
+        }
+
+    template<class T>
+        T add( T a, T b )
+        {
+            return a + b;
+        }
+
+    template<class T>
+        T subtract( T a, T b )
+        {
+            return a - b;
+        }
+
+    template<class T>
+        T multiply( T a, T b )
+        {
+            return a * b;
+        }
 };
 
 
