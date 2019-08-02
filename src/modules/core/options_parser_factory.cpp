4c2c8770bfc6252770e38b5082d6a5687fab762a
Bob Costas <finkzane@gmail.com>
2019-08-02 09:48:42 -0700

    added output of currently available modules

--- src/modules/core/options_parser_factory.cpp
+++ src/modules/core/options_parser_factory.cpp
@@ -19,6 +19,9 @@ options_parser *options_parser_factory::create( int argc, char ***argv )
                 {
                     std::cout << desc << "\n";
                     std::cout << "\nUSAGE: pep_sirf [ --help | module_name <module_args*> ] " << "\n";
+                    std::cout << "The currently available modules are:\n";
+                    std::cout << " -demux\n";
+                    std::cout << " -deconv\n\n";
                     std::cout << "--help, -h displays this message, while 'pep_sirf module_name --help' will display the help for " 
                         "the module module_name.\n";
                     return nullptr;
