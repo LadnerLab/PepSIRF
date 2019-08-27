7a7fe7640d2d336434c57c5ee8acda0179cba0ac
Bob Costas <finkzane@gmail.com>
2019-08-27 12:40:09 -0700

    added docs for options_parser_normalize class

--- include/modules/normalize/options_parser_normalize.h
+++ include/modules/normalize/options_parser_normalize.h
@@ -3,11 +3,26 @@
 #include "options_parser.h"
 #include "options.h"
 
+/**
+ * Options parser for the normalization module.
+ **/
 class options_parser_normalize : public options_parser
 {
  public:
+
+    /**
+     * Default constructor.
+     **/
     options_parser_normalize();
 
+    /**
+     * Parse the options provided by the command-line, which are 
+     * stored in argc/argv. Values are stored in opts. 
+     * @param argc The number of args.
+     * @param argv Pointer to argv.
+     * @param opts Pointer to options object 
+     * @returns boolean true if normalization should happen, false otherwise.
+     **/
     bool parse( int argc, char ***argv, options *opts );
 
 
