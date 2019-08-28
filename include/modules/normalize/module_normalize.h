b661822948e1d5716fbdc1618cc0dac0400acdae
Bob Costas <finkzane@gmail.com>
2019-08-28 16:24:31 -0700

    added peptide_score and its more-specific types

--- include/modules/normalize/module_normalize.h
+++ include/modules/normalize/module_normalize.h
@@ -2,9 +2,23 @@
 #define MODULE_NORMALIZE_HH_INCLUDED
 
 #include <string>
+#include <vector>
 
 #include "module.h"
 #include "options.h"
+
+/**
+ * Alias struct peptide_score_data to a type that is 'sample major' (column major with 
+ * respect to the input file)
+ **/
+typedef struct peptide_score_data peptide_score_data_sample_major;
+
+/**
+ * Alias struct peptide_score_data to a type that is 'peptide major' (row major with 
+ * respect to the input file)
+ **/
+
+typedef struct peptide_score_data peptide_score_data_peptide_major;
 
 /**
  * Class for the PepSIRF normalization module.
@@ -21,6 +35,36 @@ class module_normalize : public module
 
     void run( options *opts );
 
+
 };
+
+/**
+ * A struct to store peptide score data.
+ * This struct is designed to represent 
+ * all of the data present in a file that is output by 
+ * the demultiplexing module.
+ **/
+struct peptide_score_data
+{
+    /**
+     * A vector of vectors of scores. 
+     * If this struct is peptide-major, scores[ x ][ y ] will 
+     * return the score of peptide x in sample y.
+     **/
+    std::vector<std::vector<std::size_t>> scores;
+
+    /**
+     * The names of the peptides, in order in which they were
+     * found. 
+     **/
+    std::vector<std::string> pep_names;
+
+    /**
+     * The names of the samples, in order in which 
+     * they were found.
+     **/
+    std::vector<std::string> sample_names;
+};
+
 
 #endif // MODULE_NORMALIZE_HH_INCLUDED
