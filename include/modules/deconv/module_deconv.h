ceaf61dbe7de3649f3464db60b0a03a8c41d2429
Bob Costas <finkzane@gmail.com>
2019-09-19 12:59:40 -0700

    some enums were moved to their own header files

--- include/modules/deconv/module_deconv.h
+++ include/modules/deconv/module_deconv.h
@@ -13,145 +13,8 @@
 #include "maps.h"
 #include "util.h"
 #include "setops.h"
-
-/**
- * Used for data that is relevant to determining and 
- * communicating data about ties.
- **/
-namespace tie_data
-{
-    /**
-     * Enum declaring the types of ties that 
-     * can be considered. Here we consider 
-     * 3 types.
-     **/
-    enum tie_type
-    {
-        /**
-         * A single-way tie happens when only one species 
-         * meets the criteria that considers ties.
-         **/
-        SINGLE_WAY_TIE = 0,
-
-        /**
-         * A two-way tie happens when exactly two species
-         * meet the criteria that considers ties.
-         **/
-        TWO_WAY_TIE,
-
-        /**
-         * A k-way tie happens when at least three species
-         * meet the criteria that considers ties.
-         **/
-        K_WAY_TIE
-    };
-
-};
-
-/**
- * Defines certain evaluation strategies for
- * handling different situations. 
- * If we need to score or filter species 
- * we have different ways we can do that. 
- **/
-namespace evaluation_strategy
-{
-    /**
-     * Strategy for scoring species. 
-     **/
-    enum score_strategy
-    {
-        /**
-         * For integer scoring, the score of 
-         * each species is the number of peptides 
-         * that species shares a kmer with.
-         **/
-        INTEGER_SCORING = 0,
-
-        /**
-         * For fractional scoring,
-         * each species is given a score based 
-         * on the ratio of species its shared peptides
-         * share a kmer with. So a peptide that shares a kmer
-         * with 3 species is given a score of 1/3, and a peptide
-         * who only shares a kmer with a given species is given a 
-         * score of 1/1. In this way peptides that are more unique to
-         * a species are given a higher score.
-         **/
-        FRACTIONAL_SCORING,
-
-        /**
-         * Summation scoring is used in conjuction 
-         * with peptides that have count information.
-         * So if species1 shares 7 kmers with a peptide,
-         * then that species is given a score of 7. 
-         * Note that this is done for each peptide a species 
-         * shares kmers with and the species is given the score
-         * of the sum of these scores.
-         **/
-        SUMMATION_SCORING
-    };
-
-    /**
-     * Strategies for filtering peptides, 
-     * i.e. if either a species' score or 
-     * count falls below a threshold that 
-     * species is removed from consideration.
-     **/
-    enum filter_strategy
-    {
-        /**
-         * Species whose scores fall below
-         * a certain threshold will be removed.
-         **/
-        SCORE_FILTER = 0,
-
-        /**
-         * Species whose count falls 
-         * below a certain threshold will be 
-         * removed.
-         **/
-        COUNT_FILTER
-    };
-
-    /**
-     * Strategies specifying how to break ties between species. 
-     * Generally one of two outcomes will result from a tie:
-     * -Two species are tied and have enough overlapped peptides to be reported 
-     *  together. 
-     * -Two or more species are tied and the species that shares 
-     * the smallest overlap with others is reported.
-     **/
-    enum tie_eval_strategy
-    {
-        /**
-         * Species that share a certain percentage
-         * of their kmers are reported together, or 
-         * if they do not meet a given threshold they are reported
-         * separately.
-         **/
-        PERCENT_TIE_EVAL = 0,
-
-        /**
-         * Species that share a certain number of 
-         * peptides are considered tied.
-         **/
-        INTEGER_TIE_EVAL,
-
-        /**
-         * Species must have a number of 
-         * unique shared peptides
-         **/
-        SUMMATION_SCORING_TIE_EVAL
-    };
-
-    enum tie_distance_strategy
-    {
-        INTEGER_DISTANCE = 0,
-        RATIO_DISTANCE
-    };
-
-}; //namespace evaluation_strategy
+#include "evaluation_strategy.h"
+#include "tie_data.h"
 
 /**
  * The species deconvolution module of the 
@@ -325,7 +188,7 @@ class module_deconv : public module
      *          a count as determined by ev_strat.
      **/
     template<template<typename...> class MapType>
-        bool 
+        bool
         sufficient_overlap(  MapType<std::string,std::vector<std::string>>&
                              id_peptide_map,
                              MapType<std::string,MapType<std::string,std::size_t>>&
