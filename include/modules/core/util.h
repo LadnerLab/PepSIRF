ffa380e2679fb9d17df1c759c7df220beeee84c1
Bob Costas <finkzane@gmail.com>
2019-09-24 10:04:24 -0700

    moved functions to util

--- include/modules/core/util.h
+++ include/modules/core/util.h
@@ -105,6 +105,95 @@ namespace util
                     dest.emplace_back( d( compare, *it ) );
                 }
         }
+
+    template<typename A,typename B,
+        typename compare_a,
+        typename compare_b
+            >
+        bool
+        pair_positional_compare( const std::pair<A,B>& first,
+                                 const std::pair<A,B>& second,
+                                 compare_a a_comp,
+                                 compare_b b_comp
+                               )
+        {
+            return a_comp( first.first, second.first )
+                   && b_comp( first.second, second.second );
+        }
+
+/**
+ * Compare pairs in non-decreasing order,
+ * i.e. for x[ i ], x[ j ] when i < j, then 
+ * x[ i ].second >= x[ j ].second
+ * 
+ * @param first the first pair to check
+ * @param second the second pair to check
+ * @note operator '>' must be defined for type V
+ * @returns true if first.second > second.first
+ **/
+template <class K, class V>
+struct compare_pair_non_increasing
+{
+    bool operator()( const std::pair<K,V>& first,
+                     const std::pair<K,V>& second
+                   )
+    {
+        return first.second > second.second;
+    }
+};
+
+/**
+ * Compare pairs in non-increasing order,
+ * i.e. for x[ i ], x[ j ] when i < j, then 
+ * x[ i ] <= x[ j ]
+ * @param first the first pair to check
+ * @param second the second pair to check
+ * @note operator '<' must be defined for type V
+ * @returns true if first.second < second.first
+ **/
+template <class K, class V>
+struct compare_pair_non_decreasing
+{
+    bool operator()( const std::pair<K,V>& first,
+                     const std::pair<K,V>& second
+                   )
+    {
+        return std::get<1>( first ) < std::get<1>( second );
+    }
+};
+
+/**
+ * Returns euclidean distance between 
+ * 1-dimensional points a and b, i.e. a - b.
+ * @param a Item of type V
+ * @param b Item of type V
+ * @returns a - b
+ **/
+template<class V>
+struct difference
+{
+    V operator()( const V a, const V b )
+    {
+        return a - b;
+    }
+};
+
+/**
+ * Returns a / b if a < b,
+ * b / a otherwise.
+ **/
+template<class V>
+struct ratio
+{
+    V operator()( const V a, const V b )
+    {
+        // we return something <= 1.0, so
+        // want to make sure the larger is in the
+        // denominator
+        return a > b ? b / a : a / b;
+    }
+};
+
 };
 
 
