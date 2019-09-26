77acf6d77bfb96d2b048a5d42ee25656cff9a3cb
Bob Costas <finkzane@gmail.com>
2019-09-26 10:02:28 -0700

    moved functions related to distance to dist_tools

---
+++ include/modules/core/dist_tools.h
@@ -0,0 +1,68 @@
+#ifndef DIST_TOOLS_HH_INCLUDED
+#define DIST_TOOLS_HH_INCLUDED
+namespace distance_tools
+{
+    /**
+     * Calculate the distance between a member
+     * of the type T with all items between
+     * begin and end. Store the distances in a 
+     * structure that holds DistanceType objects.
+     * @param A structure that holds members of type DistanceType,
+     *        which is the type returned by the Distance
+     *        function d. Results will be stored in 
+     *        the same order in which they were found.
+     * @param begin The first item to compare with.
+     * @param end The last item to compare. 
+     * @param compare The item whose distance 
+     *        from all items between begin and end
+     *        (inclusive) is to be computed.
+     * @param d The distance func, which takes two 
+     *        members of type T and returns their distance,
+     *        a member of type DistanceType.
+     **/
+    template<template<typename...> class DestType,
+        class InputIt,
+        class T,
+        class DistanceType,
+        class DistanceCalc
+        >
+        void
+        all_distances( DestType<DistanceType>& dest,
+                       InputIt begin, InputIt end,
+                       T& compare,
+                       DistanceCalc d
+                     )
+        {
+            for( auto it = begin; it < end; ++it )
+                {
+                    dest.emplace_back( d( compare, *it ) );
+                }
+        }
+
+    template<template<typename...> class DestType,
+        class InputIt,
+        class DistanceType,
+        class DistanceCalc
+        >
+        void 
+        pairwise_distances_symmetry_optimized( DestType<DestType<DistanceType>>& dest,
+                                               InputIt begin,
+                                               InputIt end,
+                                               DistanceCalc distance
+                                             )
+            {
+                std::size_t index = 0;
+                for( auto current = begin; current != end; ++current, ++index )
+                    {
+                        all_distances( dest[ index ],
+                                       begin + index + 1,
+                                       end,
+                                       *current,
+                                       distance
+                                     );
+                    }
+            }
+
+
+}; // namespace distance_tools 
+#endif 
