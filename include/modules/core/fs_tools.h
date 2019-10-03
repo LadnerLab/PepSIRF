a28b4eaa59388a640fbb128457cb2cb7879695b1
Bob Costas <finkzane@gmail.com>
2019-10-03 11:46:23 -0700

    inclusion of fs_tools

---
+++ include/modules/core/fs_tools.h
@@ -0,0 +1,13 @@
+#ifndef FS_TOOLS_HH_INCLUDED
+#define FS_TOOLS_HH_INCLUDED
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+
+namespace fs_tools
+{
+    using namespace boost::filesystem;
+
+};
+
+
+#endif // FS_TOOLS_HH_INCLUDED
