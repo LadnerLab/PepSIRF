3c2976a26bc741c9f8c0bd78c4abb1138d8b2f9e
Bob Costas <finkzane@gmail.com>
2019-10-03 12:46:16 -0700

    added create_fname method to recurively append items to a filename

--- include/modules/core/fs_tools.h
+++ include/modules/core/fs_tools.h
@@ -13,6 +13,51 @@ namespace fs_tools
     std::string& to_dir_name( std::string& dest,
                               const std::string& str
                             );
+
+    template<typename T>
+        std::string& create_fname( std::string& dest,
+                                   const std::string& path_base,
+                                   T next
+                                 )
+        {
+            std::string path_cpy = path_base;
+
+            if( dest.length() == 0 )
+                { 
+                    if( !has_trailing_slash( path_cpy ) )
+                        {
+                            to_dir_name( path_cpy, path_base );
+                        }
+                    dest += path_cpy;
+                }
+
+            dest += boost::lexical_cast<std::string>( next );
+
+            return dest;
+        }
+
+    template<typename T, typename... Args>
+        std::string& create_fname( std::string& dest,
+                                   const std::string& path_base,
+                                   T next,
+                                   Args... remaining 
+                                 )
+        {
+            std::string path_cpy = path_base;
+            if( dest.length() == 0 )
+                { 
+                    if( !has_trailing_slash( path_cpy ) )
+                        {
+                            to_dir_name( path_cpy, path_base );
+                        }
+                    dest += path_cpy;
+                }
+
+            dest += boost::lexical_cast<std::string>( next );
+
+            return create_fname( dest, path_cpy, remaining... );
+        }
+
 };
 
 
