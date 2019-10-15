8b031c90a3e13f4c3a6807248fb6a6833a5cc97a
Bob Costas <finkzane@gmail.com>
2019-10-15 12:56:51 -0700

    added methods for templated peptide

--- include/modules/deconv/scored_peptide.h
+++ include/modules/deconv/scored_peptide.h
@@ -1,9 +1,38 @@
 #ifndef SCORED_PEPTIDE_HH_INCLUDED
 #define SCORED_PEPTIDE_HH_INCLUDED
+#include "peptide.h"
 
-class scored_peptide
+template<typename score_type>
+class scored_peptide : public peptide
 {
+ public:
+    scored_peptide() = default;
 
+    scored_peptide( const std::string& name,
+                    const std::string& pep,
+                    const score_type start_sc
+               )
+        : peptide( name, pep ), score( start_sc ) {}
+
+    scored_peptide( const std::string& pep,
+                    const score_type start_sc
+                  )
+        : peptide( pep ), score( start_sc ) {}
+    
+
+
+    score_type get_score()
+    {
+        return score;
+    }
+
+    void set_score( score_type new_sc )
+    {
+        score = new_sc;
+    }
+
+ private: 
+    score_type score;
 
 };
 
