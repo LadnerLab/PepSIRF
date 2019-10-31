002798e436e3613815a4271fb638dfbd4746d658
Bob Costas <finkzane@gmail.com>
2019-10-31 12:31:58 -0700

    creation of test_utils file

---
+++ test/test_utils.h
@@ -0,0 +1,18 @@
+#ifndef TEST_UTILS_H_INCLUDED
+#define TEST_UTILS_H_INCLUDED
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+
+#include "scored_entity.h"
+
+
+namespace test_utils
+{
+
+    
+}; // namespace test_utils
+
+
+#endif // TEST_UTILS_H_INCLUDED
