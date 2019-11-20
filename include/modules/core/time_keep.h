ac4013b430dee82b16a991fed447668639a7e0ed
Bob Costas <finkzane@gmail.com>
2019-11-20 16:12:15 -0700

    added state to time_keep

--- include/modules/core/time_keep.h
+++ include/modules/core/time_keep.h
@@ -1,24 +1,34 @@
 #ifndef TIME_KEEP_HH_INCLUDED
 #define TIME_KEEP_HH_INCLUDED
+#include "omp_opt.h"
 
 namespace time_keep
 { 
 
 /**
- * A struct to keep track of time that has elapsed.
+ * A struct to keep track of wall clock time that has elapsed.
  **/
 struct timer
 {
-    double start;
-    double end;
+    double begin_t;
+    double end_t;
+
+    void start() { begin_t = omp_get_wtime(); };
+    void stop()   { end_t = omp_get_wtime(); };
+    double get_elapsed() const { return end_t - begin_t; };
+
 };
 
  double get_elapsed( const struct timer& time );
 }
 
+/**
+ * Get the elapsed time (in seconds) of the timer. 
+ * @param time The timer from which to get elapsed time.
+ **/
 inline double time_keep::get_elapsed( const struct time_keep::timer& time )
 {
-    return time.end - time.start; 
+    return time.get_elapsed();
 }
 
 #endif // TIME_KEEP_HH_INCLUDED
