f9621314d2414df756adea476e5b0904730622f5
Bob Costas <finkzane@gmail.com>
2019-11-25 09:52:35 -0700

    added member-level documentation, output_fname member to options_normalize

--- include/modules/normalize/options_normalize.h
+++ include/modules/normalize/options_normalize.h
@@ -8,11 +8,30 @@
 class options_normalize : public options
 {
  public:
+
+    /**
+     * Sets the name of this module to "norm"
+     **/
     options_normalize();
 
+    /**
+     * Returns a string containing the arguments 
+     * supplied to the program in a vertically-aligned
+     * fashion. 
+     **/
     std::string get_arguments();
 
+    /**
+     * Name of the file that contains the 
+     * scores for peptides within a sample.
+     **/
     std::string peptide_scores_fname;
+
+    /**
+     * The name of the file to write the module's
+     * output to.
+     **/
+    std::string output_fname;
 
 
 };
