d37bb9bae56e8db1fed81c4582600ab146b65c30
Bob Costas <finkzane@gmail.com>
2019-12-03 12:35:26 -0700

    implemented constructor/destructor

--- include/modules/core/module_details.h
+++ include/modules/core/module_details.h
@@ -1,6 +1,7 @@
 #ifndef MODULE_DETAILS_HH_INCLUDED
 #define MODULE_DETAILS_HH_INCLUDED
 #include <string>
+#include <stdexcept>
 
 #include "options.h"
 #include "module.h"
@@ -32,7 +33,15 @@
 class module_details
 {
  public:
+
+    /**
+     * Construct the module_details, initialize everything to null.
+     **/
     module_details();
+
+    /**
+     * Destroy the initializer, deletes any allocated memory.
+     **/
     ~module_details();
     void initialize( const std::string& module_name );
 
@@ -41,11 +50,21 @@ class module_details
     options_parser *get_options_parser();
 
  private:
-    options *opts;
-    module *mod;
-    options_parser *opt_parser;
-    bool initialized;
 
+    /**
+     * A pointer to a subclass of options.
+     **/
+    options *opts;
+
+    /**
+     * A pointer to a subclass of module.
+     **/
+    module *mod;
+
+    /**
+     * A pointer to a subclass of options_parser
+     **/
+    options_parser *opt_parser;
 
 
 };
--- src/modules/core/module_details.cpp
+++ src/modules/core/module_details.cpp
@@ -1,15 +1,22 @@
 #include "module_details.h"
+#include <algorithm>
 
 module_details::module_details() 
-    : opts( nullptr ), mod( nullptr ), opt_parser( nullptr ),
-      initialized( false ) {} 
+    : opts( nullptr ), mod( nullptr ), opt_parser( nullptr )
+      {} 
 
 module_details::~module_details() 
 {
-    if( initialized )
+    if( opts )
         {
             delete opts;
+        }
+    if( mod )
+        {
             delete mod;
+        }
+    if( opt_parser )
+        {
             delete opt_parser;
         }
 }
