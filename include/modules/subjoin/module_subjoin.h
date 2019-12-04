c0cea4e258386a74e8c842e5dcf0e72f53d9f957
Bob Costas <finkzane@gmail.com>
2019-12-04 15:23:37 -0700

    creation of subjoin files

---
+++ include/modules/subjoin/module_subjoin.h
@@ -0,0 +1,16 @@
+#ifndef MODULE_SUBJOIN_HH_DEFINED
+#define MODULE_SUBJOIN_HH_DEFINED
+
+#include "module.h"
+
+/**
+ *
+ **/
+class module_subjoin : public module
+{
+ public:
+    module_subjoin();
+
+};
+
+#endif // MODULE_SUBJOIN_HH_INCLUDED
---
+++ src/modules/subjoin/module_subjoin.cpp
@@ -0,0 +1,3 @@
+#include "module_subjoin.h"
+
+module_subjoin::module_subjoin() = default;
