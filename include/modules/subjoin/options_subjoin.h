496df7f6eae2d0ebd0e33b89670b38d20e7250fd
Bob Costas <finkzane@gmail.com>
2019-12-05 10:50:45 -0700

    created optiosn_subjoin class

---
+++ include/modules/subjoin/options_subjoin.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONS_SUBJOIN_HH_INCLUDED
+#define OPTIONS_SUBJOIN_HH_INCLUDED
+#include "options.h"
+
+class options_subjoin : public options
+{
+    options_subjoin();
+};
+
+#endif // OPTIONS_SUBJOIN_HH_INCLUDED
---
+++ src/modules/subjoin/options_subjoin.cpp
@@ -0,0 +1,3 @@
+#include "options_subjoin.h"
+
+options_subjoin::options_subjoin() = default;
