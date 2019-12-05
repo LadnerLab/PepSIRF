6f060d69abb3dabe6bb8087faffd28b94dc8d3c8
Bob Costas <finkzane@gmail.com>
2019-12-05 11:20:55 -0700

    added output of help message

--- src/modules/subjoin/options_parser_subjoin.cpp
+++ src/modules/subjoin/options_parser_subjoin.cpp
@@ -9,6 +9,29 @@ bool options_parser_subjoin::parse( int argc, char ***argv, options *opts )
     po::variables_map vm;
 
 
+    po::options_description desc( "PepSIRF: Peptide-based Serological Immune "
+                                  "Response Framework subjoin module"
+                                );
 
-    return false;
+    desc.add_options()
+        (
+         "help,h", "Produce help message\n"
+        );
+
+
+    po::store( po::command_line_parser( argc, *argv ).options( desc ).run(), vm);
+
+    if( vm.count( "help" ) 
+	    || argc == 2 
+	  )
+        {
+            std::cout << desc << std::endl;
+            return false;
+        }
+    else
+        {
+            po::notify( vm );
+            return true;
+        }
+
 }
