f79bc66bc35946665d92d0297f82dd09a0f3db8d
Bob Costas <finkzane@gmail.com>
2019-12-17 11:05:44 -0700

    added run, parse_namelist methods

--- include/modules/subjoin/module_subjoin.h
+++ include/modules/subjoin/module_subjoin.h
@@ -1,16 +1,37 @@
 #ifndef MODULE_SUBJOIN_HH_DEFINED
 #define MODULE_SUBJOIN_HH_DEFINED
+#include<fstream>
 
 #include "module.h"
 #include "options_subjoin.h"
+#include "peptide_scoring.h"
 
 /**
- *
+ * A module to join multiple score matrices into one.
+ * Filters can be included to determine which peptide names will be output.
  **/
 class module_subjoin : public module
 {
  public:
     module_subjoin();
+
+    /**
+     * Run the subjoin module.
+     **/
+    void run( options *opts );
+
+    /**
+     * Parse a list of names from the input stream.
+     * There should be one name per line in the stream.
+     * @param dest The location to store names, 
+     *        this method will store items found in file at 
+     *        the end of the vector.
+     * @param file A stream to read names from.
+     **/
+    void parse_namelist( std::vector<std::string>& dest,
+                         std::istream& file
+                       );
+
 
 };
 
--- src/modules/subjoin/module_subjoin.cpp
+++ src/modules/subjoin/module_subjoin.cpp
@@ -1,3 +1,38 @@
 #include "module_subjoin.h"
+#include "matrix.h"
 
+#include <iostream>
 module_subjoin::module_subjoin() = default;
+
+void module_subjoin::parse_namelist( std::vector<std::string>& dest,
+                                     std::istream& file
+                                   )
+{
+    std::string line;
+    while( std::getline( file, line ).good() )
+        {
+            dest.push_back( line );
+        }
+}
+
+void module_subjoin::run( options *opts )
+{
+    options_subjoin *s_opts = (options_subjoin*) opts;
+    std::ifstream names_list( s_opts->names_list_fname,
+                              std::ios_base::in
+                            );
+
+    std::vector<std::string> peptide_name_list;
+
+    parse_namelist( peptide_name_list, names_list );
+
+    peptide_score_data_sample_major original_scores;
+    peptide_scoring::parse_peptide_scores( original_scores,
+                                           s_opts->in_matrix_fname
+                                         );
+
+    original_scores.scores = original_scores.scores.filter_rows( peptide_name_list );
+    original_scores.pep_names = peptide_name_list;
+    peptide_scoring::write_peptide_scores( s_opts->out_matrix_fname, original_scores );
+
+}
