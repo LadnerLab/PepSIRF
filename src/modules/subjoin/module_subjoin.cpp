b792baabdcbc4f14c8d270627d4ed0ec2492c480
Bob Costas <finkzane@gmail.com>
2019-12-18 14:06:12 -0700

    Multiple files and namelist can now be subjoined

--- src/modules/subjoin/module_subjoin.cpp
+++ src/modules/subjoin/module_subjoin.cpp
@@ -1,6 +1,7 @@
 #include "module_subjoin.h"
 #include "matrix.h"
 
+#include <omp.h>
 #include <iostream>
 module_subjoin::module_subjoin() = default;
 
@@ -18,21 +19,53 @@ void module_subjoin::parse_namelist( std::vector<std::string>& dest,
 void module_subjoin::run( options *opts )
 {
     options_subjoin *s_opts = (options_subjoin*) opts;
-    std::ifstream names_list( s_opts->names_list_fname,
-                              std::ios_base::in
-                            );
+    std::vector<peptide_score_data_sample_major>
+        parsed_score_data;
+    parsed_score_data.resize( s_opts->matrix_name_pairs.size() );
+    std::uint32_t idx = 0;
 
-    std::vector<std::string> peptide_name_list;
+    // for all of the ( score_matrix, names_to_filter ) pairs:
+    for( idx = 0; idx < s_opts->matrix_name_pairs.size(); ++idx )
+        {
+            auto &score_name_pair = s_opts->matrix_name_pairs[ idx ];
+            peptide_score_data_sample_major&
+                my_data = parsed_score_data[ idx ];
+                
+            std::string& matrix_name_list  = score_name_pair.first;
+            std::ifstream names_list( score_name_pair.second,
+                                      std::ios_base::in
+                                     );
+            // parse the matrix
+            peptide_scoring
+                ::parse_peptide_scores( my_data,
+                                        matrix_name_list
+                                      );
 
-    parse_namelist( peptide_name_list, names_list );
+        // parse the peptide scores
+            std::vector<std::string> peptide_name_list;
+            parse_namelist( peptide_name_list, names_list );
+        // filter the data, assign to the scores and peptide name list
+            my_data.scores = my_data.scores.filter_rows( peptide_name_list );
+            my_data.pep_names = peptide_name_list;
+        }
 
-    peptide_score_data_sample_major original_scores;
-    peptide_scoring::parse_peptide_scores( original_scores,
-                                           s_opts->in_matrix_fname
-                                         );
+    std::ofstream output( s_opts->out_matrix_fname, std::ios_base::out );
 
-    original_scores.scores = original_scores.scores.filter_rows( peptide_name_list );
-    original_scores.pep_names = peptide_name_list;
-    peptide_scoring::write_peptide_scores( s_opts->out_matrix_fname, original_scores );
+    if( parsed_score_data.size() > 1 )
+        {
 
+            labeled_matrix<double,std::string> joined_matrix;
+            // join all of the matrices
+            for( uint idx = 0; idx < parsed_score_data.size(); ++idx )
+                {
+                    joined_matrix = joined_matrix
+                        .full_outer_join( parsed_score_data[ idx ].scores );
+                }
+
+            output << joined_matrix;
+        }
+    else
+        {
+            output << parsed_score_data[ 0 ].scores;
+        }
 }
