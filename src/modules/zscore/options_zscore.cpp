d7e414ed55b2f06d9ea932bba2ae22944c7e0414
Bob Costas <finkzane@gmail.com>
2019-12-27 14:23:28 -0700

    added options for the zscore module

---
+++ src/modules/zscore/options_zscore.cpp
@@ -0,0 +1,18 @@
+#include "options_zscore.h"
+#include <sstream>
+
+options_zscore::options_zscore() = default;
+
+std::string options_zscore::get_arguments()
+{
+
+    std::ostringstream str_stream;
+
+    str_stream << "--input       " << in_fname << "\n "
+               << "--output      " << out_fname << "\n "
+               << "--num_threads " << num_threads << "\n "
+               << "\n";
+          
+
+    return str_stream.str();
+}
