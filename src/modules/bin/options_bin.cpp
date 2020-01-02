8b51998bd18435676a128ffd2f628ae18f2649e4
Bob Costas <finkzane@gmail.com>
2020-01-02 13:53:10 -0700

    added bin options, output of arguments

--- src/modules/bin/options_bin.cpp
+++ src/modules/bin/options_bin.cpp
@@ -1,11 +1,19 @@
 #include "options_bin.h"
 #include <sstream>
 
-options_bin::options_bin() = default;
+options_bin::options_bin()
+    : DEFAULT_MIN_BIN_SIZE{ 300 },
+      DEFAULT_ROUNDING_FACTOR{ 0 } {}
 
 std::string options_bin::get_arguments()
 {
     std::ostringstream str_stream;
+
+    str_stream << "--scores   " << input_scores_fname << "\n " 
+               << "--bin_size " << min_bin_size << "\n "
+               << "--round_to " << rounding_factor << "\n "
+               << "--output   " << output_bins_fname << "\n"
+        ;
 
     return str_stream.str();
 };
