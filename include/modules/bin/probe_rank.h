741cedc2cdd9dc6483876cad26f6b7768c3d5061
Bob Costas <finkzane@gmail.com>
2020-01-03 12:58:25 -0700

    added rank_probe method

--- include/modules/bin/probe_rank.h
+++ include/modules/bin/probe_rank.h
@@ -25,9 +25,24 @@ class probe_rank
     /**
      * Constructor that sets the rounding factor of 
      * this object. 
+     * @param rounding_factor the rounding factor to use when 
+     *        ranking probes based upon their scores.
+     *        Each score is rounded to the nearest 1/10^x for 
+     *        rounding factor x.
      **/
     probe_rank( const std::size_t rounding_factor )
         : rounding_factor{ rounding_factor } {}
+
+    /**
+     * Rank a probe given its score.
+     * Scores with the same rank (score rounded to the nearest 
+     * 1/10^x for rounding_factor x) are stored together.
+     * @param score The score of the probe to rank
+     * @param probe The probe to rank
+     **/
+    void rank_probe( const score_type score,
+                     const probe_type probe
+                   );
 
 
  private:
--- src/modules/bin/probe_rank.cpp
+++ src/modules/bin/probe_rank.cpp
@@ -9,3 +9,17 @@ double probe_rank::round_to_factor( const double value ) const
     const int adjustment = std::pow( 10, rounding_factor );
     return std::floor( value * ( adjustment ) + 0.5 ) / adjustment;
 }
+
+void probe_rank::rank_probe( const score_type score,
+                             const probe_type probe
+                           )
+{
+    double rounded_score = round_to_factor( score );
+
+    auto insert_loc = ranked_probes.insert( std::make_pair( rounded_score,
+                                                            std::vector<probe_type>()
+                                                          )
+                                          );
+
+    insert_loc.first->second.emplace_back( probe );
+}
