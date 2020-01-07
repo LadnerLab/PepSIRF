691361e97c933102595c5af5447c855b0ec988dc
Bob Costas <finkzane@gmail.com>
2020-01-07 14:45:15 -0700

    added first steps of module_s_enrich::run

--- src/modules/s_enrich/module_s_enrich.cpp
+++ src/modules/s_enrich/module_s_enrich.cpp
@@ -1,7 +1,25 @@
+#include <iostream>
+
 #include "module_s_enrich.h"
+#include "peptide_scoring.h"
+#include "time_keep.h"
+#include "omp_opt.h"
 
 void module_s_enrich::run( options *opts )
 {
     options_s_enrich *e_opts = (options_s_enrich*) opts;
+    time_keep::timer timer;
+    timer.start();
 
+
+    peptide_score_data_sample_major zscores;
+    peptide_score_data_sample_major norm_scores;
+
+    peptide_scoring::parse_peptide_scores( zscores, e_opts->in_zscore_fname );
+    peptide_scoring::parse_peptide_scores( zscores, e_opts->in_norm_score_fname );
+
+    timer.stop();
+
+    
+    std::cout << "Took " << timer.get_elapsed() << " seconds.\n";
 }
