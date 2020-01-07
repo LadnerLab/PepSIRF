68e4111cca19fce11163645ed54ee56120925d36
Bob Costas <finkzane@gmail.com>
2020-01-07 12:56:22 -0700

    initialized s_enrich module

---
+++ include/modules/s_enrich/module_s_enrich.h
@@ -0,0 +1,14 @@
+#ifndef MODULE_S_ENRICH_HH_INCLUDED
+#define MODULE_S_ENRICH_HH_INCLUDED
+#include "module.h"
+#include "options_s_enrich.h"
+
+class module_s_enrich : public module
+{
+public:
+
+    void run( options *opts );
+
+};
+
+#endif // MODULE_S_ENRICH_HH_INCLUDED 
---
+++ src/modules/s_enrich/module_s_enrich.cpp
@@ -0,0 +1,7 @@
+#include "module_s_enrich.h"
+
+void module_s_enrich::run( options *opts )
+{
+    options_s_enrich *e_opts = (options_s_enrich*) opts;
+
+}
