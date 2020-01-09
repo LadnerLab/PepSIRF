6b165af160e2421f02aa1297d43d4cd47a16ed79
Bob Costas <finkzane@gmail.com>
2020-01-09 14:01:27 -0700

    added help information for get_enrichment_candidates

--- include/modules/s_enrich/module_s_enrich.h
+++ include/modules/s_enrich/module_s_enrich.h
@@ -132,6 +132,25 @@ public:
     void run( options *opts );
 
     /**
+     * Get the score data about a peptide, returning each of its scores in 
+     * a vector of peptide_score types. 
+     * @param zscore_data contains data for peptide zscores.
+     * @param norm_score_data Data containing normalized scores for each peptide
+     * @param raw_score_data Data containing raw scores for each peptide.
+     * @pre zscore_data, norm_score_data, and (if included) raw_score_data 
+     *      all contain data for the same peptides and samples.
+     * @pre zscore_data and norm_score_data are not null
+     * @note if raw_score_data is null, the return peptide_scores will 
+     *       have their raw_score member set to 0.
+     **/
+    std::vector<peptide_score<std::string>>
+    get_enrichment_candidates( const peptide_score_data_sample_major *zscore_data,
+                               const peptide_score_data_sample_major *norm_score_data,
+                               const peptide_score_data_sample_major *raw_score_data,
+                               const std::size_t sample_idx
+                             );
+
+    /**
      * Write the names of probes to an output stream, one per line..
      * @param stream The stream to write probe names to
      * @param probes A vector of peptide_scores to whose names
