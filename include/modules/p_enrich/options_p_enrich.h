a9ea84a213ab828a99443a0a58f42cdf739c5fb2
Bob Costas <finkzane@gmail.com>
2020-01-15 16:11:27 -0700

    added options to 'options_p_enrich' class

--- include/modules/p_enrich/options_p_enrich.h
+++ include/modules/p_enrich/options_p_enrich.h
@@ -13,8 +13,29 @@ class options_p_enrich : public options
           DEFAULT_OUT_FNAME_JOIN{ "~" }
           {}
 
+    using score_type = double;
+    using score_pair = std::pair<score_type,score_type>;
+
+    std::string get_arguments();
+
+    std::string in_zscore_fname;
+    score_pair zscore_params;
+
+    std::string in_norm_scores_fname;
+    score_pair norm_scores_params;
+
+    std::string in_raw_scores_fname;
+    score_pair raw_scores_params;
+
+    std::string out_dirname;
+
+    std::string out_fname_join;
+
+    std::string out_suffix;
+
     const std::string DEFAULT_OUTPUT_DIRNAME;
     const std::string DEFAULT_OUT_FNAME_JOIN;
+
 };
 
 #endif // OPTIONS_P_ENRICH_HH_INCLUDED
