6b27cf36e7c14f468b5b4dcc0fe8357c81edd7be
Bob Costas <finkzane@gmail.com>
2020-01-15 16:12:38 -0700

    added options_parser_p_enrich class with parse method

---
+++ src/modules/p_enrich/options_parser_p_enrich.cpp
@@ -0,0 +1,14 @@
+#include "options_parser_p_enrich.h"
+
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+#include <limits>
+#include <stdexcept>
+#include <iostream>
+#include "stream_tools.h"
+
+#include "predicate.h"
+
+bool options_parser_p_enrich::parse( int argc, char ***argv, options *opts )
+{
+};
