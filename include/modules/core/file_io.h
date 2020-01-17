0112eb1adfb14a4329c1eb1cd89042996561b78f
Bob Costas <finkzane@gmail.com>
2020-01-17 13:07:19 -0700

    added description of write_file method

--- include/modules/core/file_io.h
+++ include/modules/core/file_io.h
@@ -5,21 +5,33 @@
 namespace pepsirf_io
 {
 
-template
-<typename Iterator,
- typename JoinSequence
->
-void write_file( std::ostream& output,
-                 Iterator begin,
-                 Iterator end,
-                 const JoinSequence join
-               )
-{
-    for( auto current = begin; current != end; ++current )
-        {
-            output << *current << join;
-        }
-}
+    /**
+     * Simple method to write joined items in the range [begin, end) 
+     * to an output stream.
+     * @tparam Iterator the iterator to be iterated over.
+     * @tparam JoinSequence The sequence that will be written to 
+     *         the stream between each item in the range [begin, end)
+     * @param output The ostream being written to
+     * @param begin The first item in the range
+     * @param end the last item in the range
+     * @param join The JoinSequence on which items in the range should be 
+     *        joined.
+     **/
+    template
+    <typename Iterator,
+     typename JoinSequence
+     >
+    void write_file( std::ostream& output,
+                     Iterator begin,
+                     Iterator end,
+                     const JoinSequence join
+                   )
+    {
+        for( auto current = begin; current != end; ++current )
+            {
+                output << *current << join;
+            }
+    }
 
 }; // namespace pepsirf_io
 
