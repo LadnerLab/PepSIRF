4c3dc03eae6c6dabb84faf3373cf2d6a3524894e
Bob Costas <finkzane@gmail.com>
2020-01-28 13:48:13 -0700

    added docs for methods, class

--- include/modules/zscore/nan_report.h
+++ include/modules/zscore/nan_report.h
@@ -4,12 +4,39 @@
 #include <cstdint>
 #include <iostream>
 
+/**
+ * A 'nan_report' contains information about a 
+ * peptide whose zscore is nan. Each report tracks 
+ * the name of the probe with a 'nan' zscore, 
+ * the name of the sample that probe is in, and the 
+ * number of the bin that probe is in.
+ **/
 struct nan_report
 {
 public:
+
+    /**
+     * The name of the probe with a 
+     * zscore of 'nan'
+     **/
     std::string probe_name;
+
+    /**
+     * The name of the sample the probe 
+     * is in.
+     **/
     std::string sample_name;
+
+    /**
+     * The bin number of the probe.
+     **/
     std::size_t bin_number;
+
+    /**
+     * Character to join on when 
+     * writing representing the report 
+     * as a string.
+     **/
     char join_char;
 
 
@@ -29,6 +56,10 @@ public:
                 ) : nan_report{ probe_name, sample_name, bin_number, '\t' }
     {}
 
+    /**
+     * Return the string representation of this 
+     * report, joined on this->join_char
+     **/
     std::string to_string() const
     {
         return probe_name + join_char
@@ -38,6 +69,12 @@ public:
 
 };
 
+/**
+ * Write the nan_report to a stream.
+ * @tparam Stream the type of strime to write the report to.
+ * @param stream the stream to write the report to
+ * @param rep The report to write to the stream.
+ **/
 template<typename Stream>
 Stream& operator<<( Stream& stream, const nan_report& rep )
 {
