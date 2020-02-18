32f9efb74532abefffa38211dc100433e457884e
Bob Costas <finkzane@gmail.com>
2020-02-18 13:34:45 -0700

    added method to detect multiple best matches

--- include/modules/demux/et_search.h
+++ include/modules/demux/et_search.h
@@ -8,22 +8,36 @@
  * error tolerance. 
  **/
 template<typename FrequencyMap,
-         bool reference_independent = false
+         bool reference_dependent = true
        >
 class et_seq_search
 {
 public:
     using FrequencyVal = typename FrequencyMap::iterator;
 
-    et_seq_search( const sequence_indexer& ref_idx )
-        : idx{ ref_idx }, counts{}
+    et_seq_search( const sequence_indexer& ref_idx,
+                   FrequencyMap& counts
+                 )
+        : idx{ ref_idx }, counts{ counts }
           
     {}
 
     /**
      *
      **/
-    FrequencyVal find( const sequence& probe,
+    template<typename Iterator,
+             typename Get
+           > 
+    bool multiple_best_matches( Iterator begin,
+                                Iterator end,
+                                Get ret
+                              )
+    {
+
+        return std::distance( begin, end ) > 1 &&
+            ret( *begin ) == ret( *(++begin) );
+    }
+
                        std::size_t hamming_tol,
                        std::size_t start,
                        std::size_t len
