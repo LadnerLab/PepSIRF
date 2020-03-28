86228c42f10739623b11099e8fdae960a2ec32c0
Bob Costas <finkzane@gmail.com>
2020-03-28 10:40:15 -0700

    overrides no longer necessary

--- src/modules/core/file_io.cpp
+++ src/modules/core/file_io.cpp
@@ -17,27 +17,3 @@ bool pepsirf_io::is_gzipped( std::istream& to_check )
 
     return is_gzipped;
 }
-
-#ifdef ZLIB_ENABLED
-
-std::ostream& pepsirf_io::gzip_writer::get_raw_stream()
-{
-    return *stream_ptr;
-}
-
-
-namespace std
-{
-    pepsirf_io::gzip_reader& getline( pepsirf_io::gzip_reader& zip, string& s )
-    {
-        char str[ 10000 ];
-        zip.getline( str, 10000 );
-
-        s = str;
-        return zip;
-    }
-
-};
-
-
-#endif
