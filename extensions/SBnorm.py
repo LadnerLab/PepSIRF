2b24c420671dafe055f4717af272088ce9efc732
jtladner <jtladner@gmail.com>
2020-08-18 21:23:39 -0700

    Update SBnorm.py
    Made changes to support using a separate matrix to supply buffer-only controls.

--- extensions/SBnorm.py
+++ extensions/SBnorm.py
@@ -12,6 +12,7 @@ def main():
     usage = '%prog [options]'
     p = optparse.OptionParser()
     p.add_option('-i', '--input',  help='Matrix of normalized (e.g., column-sum) read counts. [None]')
+    p.add_option('--negMat',  help='Optionally, this flag can be used to supply a separate data matrix containing SB samples. [None]')
     p.add_option('-s', '--start',  help='Optional approach for identifying negative controls. Can provide unique string at the start of all negative control samples [None]')
     p.add_option('-n', '--negNames', help='Optional approach for identifying negative controls. Comma-separated list of negative control samples names [None]')
     p.add_option('-a', '--approach', help='Approach for normalization. Options: "ratio" and "diff" [None, REQ]')
@@ -20,13 +21,19 @@ def main():
     
     inD = parseCounts(opts.input)
     
+    if opts.negMat:
+        sbD = parseCounts(opts.negMat)
+    
     if opts.start and opts.negNames:
         print("Script does not currently support a combination of '--start' and '--negNames', using '--negNames'")
         negL = opts.negNames.split(",")
     elif opts.negNames:
         negL = opts.negNames.split(",")
     elif opts.start:
-        negL = [x for x in inD if x.startswith(opts.start)]
+        if opts.negMat:
+            negL = [x for x in sbD if x.startswith(opts.start)]
+        else:
+            negL = [x for x in inD if x.startswith(opts.start)]
     else:
         negL = False
         print("Must use either '--start' or '--negNames' to specify the negative control samples to use for normalization")
@@ -35,7 +42,11 @@ def main():
         samps = list(inD.keys())
         negNormD = {x:{} for x in samps}
         for p in inD[samps[0]]:
-            avgNeg = np.mean([inD[x][p] for x in negL])
+            if opts.negMat:
+                avgNeg = np.mean([sbD[x][p] for x in negL])
+            else:
+                avgNeg = np.mean([inD[x][p] for x in negL])
+            
             for s in samps:
                 if opts.approach == "ratio":
                     negNormD[s][p] = inD[s][p]/avgNeg
