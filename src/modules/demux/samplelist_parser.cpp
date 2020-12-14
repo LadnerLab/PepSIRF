e1b9ba17a5b838b9871c20dc49e243b631e1de4c
vidalrmartinez <vrm55@nau.edu>
2020-12-14 10:47:51 -0700

    Updating demux samplelist structure and flexability.

--- src/modules/demux/samplelist_parser.cpp
+++ src/modules/demux/samplelist_parser.cpp
@@ -2,13 +2,15 @@
 #include <sstream>
 #include <boost/algorithm/string.hpp>
 
+// Pass not just filename, but also the string for headers.
 std::vector<sample> samplelist_parser::parse( const std::string filename )
 {
     const int FORWARD_ONLY_SIZE        = 2;
     const int FORWARD_AND_REVERSE_SIZE = 3;
 
     std::ifstream in_stream( filename );
-
+    // parse the header string if not empty, otherwise use default.
+    // Instead of these three columns, headers string will be parsed into a set.
     std::string id1  = "";
     std::string id2  = "";
     std::string name = "";
@@ -31,7 +33,11 @@ std::vector<sample> samplelist_parser::parse( const std::string filename )
             boost::trim_right( line );
             boost::split( split_line, line, boost::is_any_of( "\t" ) );
             ++line_no;
-
+            // Now it will be more nondeterministic what number of headers will be used.
+            // We must check to verify the size of split_line is equal to the size of the headers set.
+            // Read the first line and assume it to be a line of headers. Store each into an array samplefile_headers.
+            // for each header from the input file, check it is found in the set of specified headers. If not throw error.
+            // Track every index that is to be included and store for each following line those indexes into the sample object.
             if( split_line.size() == FORWARD_ONLY_SIZE )
                 {
                     id1  = split_line[ 0 ];
