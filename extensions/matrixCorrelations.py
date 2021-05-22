f96a7886e93802c8b6dfd00636c7b6fddc09ec69
jtladner <jtladner@gmail.com>
2021-05-22 13:58:03 -0700

    Update matrixCorrelations.py
    Added option for specifying type of correlation method to use.

--- extensions/matrixCorrelations.py
+++ extensions/matrixCorrelations.py
@@ -14,12 +14,13 @@ def main():
 
     p.add_option('-i', '--input',  help='Input matrix file. [None, REQ]')
     p.add_option('-o', '--output', help='Name for output correlation plot [None, REQ]')
+    p.add_option('-m', '--method', default='pearson', help='Correlation method to use. Options: ‘pearson’, ‘kendall’, ‘spearman’ [pearson]')
     p.add_option('-c', '--cmap',  default="viridis", help='Matplotlib colormap to use for plot. [viridis]')
     p.add_option('--dpi', type='int',  default=300, help='Dots per inch to use for plot. [300]')
 
     opts, args = p.parse_args()
     
-    mt.corrMat(opts.input, opts.output, cMap=opts.cmap, setdpi=opts.dpi)
+    mt.corrMat(opts.input, opts.output, cMap=opts.cmap, setdpi=opts.dpi, meth=opts.method)
 
 ###------------------------------------->>>>    
 
