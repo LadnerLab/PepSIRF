ac69b2f5c88e7cfdec0e2171074b299d108fb1ef
vidalrmartinez <vrm55@nau.edu>
2021-09-20 11:31:49 -0700

    added feature to ref-independent mode in demux. Duplicate sequences for probe identification removed from final output. These are still included in the data generation for demux.

--- include/modules/demux/module_demux.h
+++ include/modules/demux/module_demux.h
@@ -88,7 +88,7 @@ class module_demux : public module
     void write_outputs( std::string outfile_name,
                         parallel_map<sequence, std::vector<std::size_t>*>& seq_scores,
                         std::vector<sample>& samples,
-                        std::map<std::string, std::size_t> id_duplicates
+                        std::map<std::string, std::size_t> seq_duplicates
                       );
 
 
