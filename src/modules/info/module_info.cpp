e86e08edd9974431d1a49e72a3f61818d0235cf3
Johnathan Ray <jdr479@nau.edu>
2022-10-13 12:36:12 -0700

    Created flag that finds replicate averages; fixes Issue #149

--- src/modules/info/module_info.cpp
+++ src/modules/info/module_info.cpp
@@ -64,5 +64,79 @@ void module_info::run( options *opts )
 
         }
 
+    if (!i_opts.in_replicates_fname.empty() && !i_opts.out_avgs_fname.empty())
+        {
+            std::unordered_map<std::string, std::vector<int>> sequence_map = {};
+        
+            std::ifstream replicate_names{ i_opts.in_replicates_fname };
+            std::ofstream averages{ i_opts.out_avgs_fname };
+
+            averages << "Sequence name";
+
+            // Get sequence names from the name file
+            std::string current_line;
+            std::vector<std::string> lines_from_file;
+            std::vector<std::string> split_line;
+            while( std::getline( replicate_names, current_line ) )
+                {
+                    lines_from_file.emplace_back(current_line);
+                }
+        
+            // Create list of base sequence names
+            std::vector<std::string> base_sequence_names;
+            std::string delimiter = "\t";
+            std::string sequence;
+            std::vector<int> replicate_vector;
+            for( std::string line: lines_from_file )
+                {
+                    // Get base sequence from current line
+                    sequence = line.substr(0, line.find(delimiter));
+
+                    // Add to base sequence list, write to file, and place in map as key
+                    base_sequence_names.emplace_back(sequence);
+                    averages << "\t" << sequence;
+                    sequence_map.emplace(std::make_pair(sequence, replicate_vector));
+                }
+
+            averages << "\n";
+
+            for( int pep_index = 0; pep_index < scores.pep_names.size(); pep_index++ )
+                {
+                    averages << scores.pep_names[pep_index];
+                    for( int sample_index = 0; sample_index < scores.sample_names.size(); sample_index++)
+                        {
+                            for( std::string base_seq: base_sequence_names )
+                                {
+                                    if( scores.sample_names[sample_index].find(base_seq) != std::string::npos)
+                                        {
+                                            sequence_map[base_seq].emplace_back( scores.scores.at(sample_index, pep_index) );
+                                            break;
+                                        }
+                                }
+                        }
+
+                    float rep_total;
+                    float rep_avg = 0.0f;
+                    for( std::string base_seq: base_sequence_names )
+                        {
+                            rep_total = 0;
+                            for( int rep_val: sequence_map[base_seq] )
+                                {
+                                     rep_total += rep_val;
+                                }
+                            rep_avg = rep_total / (float) sequence_map[base_seq].size();
+                            averages << "\t" << rep_avg;
+                        }
+                    averages << "\n";
+                    
+                    // Reset the sequence map
+                    for( std::string base_seq: base_sequence_names )
+                        {
+                            sequence_map[base_seq].clear();
+                        }
+                }
+            
+        }
+
 
 }
