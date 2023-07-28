7a72d991d4197085315b85ed2a27d94f5cdf525e
Num0Programmer <asajh4@gmail.com>
2023-07-28 09:50:31 -0700

    Implemented error and warning functions. I'd like to come up with a way to save the file header: (1) it appears log files are just overwritten with current implementation, and (2) logfile is repeatedly opened and closed which has the potential to make modules very slow if they log a log of information.

--- src/modules/core/logger.cpp
+++ src/modules/core/logger.cpp
@@ -1,7 +1,14 @@
 #include "logger.h"
 
+// TODO: deal with repeated opening and closing log file
 void error(std::string err_str)
 {
+    std::ofstream log(options::get_logfile());
+
+    std::cout << "Error: " << err_str;
+    log << "Error: " << err_str;
+
+    log.close();
 }
 
 void info(std::string info_str)
@@ -16,5 +23,11 @@ void info(std::string info_str)
 
 void warn(std::string warn_str)
 {
+    std::ofstream log(options::get_logfile());
+
+    std::cout << "Warning: " << warn_str;
+    log << "Warning: " << warn_str;
+
+    log.close();
 }
 
