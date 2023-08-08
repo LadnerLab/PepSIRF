2bf50554725e2863b236c4402342525ae56ead58
Num0Programmer <asajh4@gmail.com>
2023-08-08 11:51:59 -0700

    Syncronized syntax and output format of 'get_arguments' method for all module options classes.

--- src/modules/zscore/options_zscore.cpp
+++ src/modules/zscore/options_zscore.cpp
@@ -6,15 +6,16 @@ options_zscore::options_zscore() = default;
 std::string options_zscore::get_arguments()
 {
     std::ostringstream str_stream;
-    str_stream << "--scores      " << in_fname << "\n "
-               << "--bins        " << in_bins_fname << "\n "
-               << "--trim        " << trim_percent << "\n "
-               << "--hdi         " << hpd_percent << "\n"
-               << "--output      " << out_fname << "\n "
-               << "--nan_report  " << nan_report_fname << "\n "
-               << "--num_threads " << num_threads << "\n "
-               << "--logfile     " << logfile << "\n "
-               << "\n";
+
+    str_stream
+        << "--scores      " << in_fname << "\n"
+        << "--bins        " << in_bins_fname << "\n"
+        << "--trim        " << trim_percent << "\n"
+        << "--hdi         " << hpd_percent << "\n"
+        << "--output      " << out_fname << "\n"
+        << "--nan_report  " << nan_report_fname << "\n"
+        << "--num_threads " << num_threads << "\n"
+        << "--logfile     " << logfile << "\n\n";
 
     return str_stream.str();
 }
