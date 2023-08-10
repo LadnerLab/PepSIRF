3034963c15a7d006e740c170e8c108b7bb541c2a
Num0Programmer <asajh4@gmail.com>
2023-08-10 10:48:43 -0700

    Syncronized options_demux.cpp and options_deconv.cpp with the other modules' options class files.

--- src/modules/deconv/options_deconv.cpp
+++ src/modules/deconv/options_deconv.cpp
@@ -5,37 +5,29 @@
 
 std::string options_deconv::get_arguments()
 {
-    if (logfile.empty())
-    {
-        logfile = set_default_log();
-    }
-
     std::ostringstream str_stream;
 
     // returns the 'string' representation of a bool
     auto bool_str = []( bool opt ) -> std::string
         { return opt == true ? "true" : "false"; };
 
-    str_stream << "--linked                  " << linked_fname  << "\n" <<  // not sure why but this one needs one space fewer
-        " --threshold               " << threshold <<  "\n" <<
-        " --enriched                " << enriched_fname <<  "\n" <<
-        " --single_threaded         " << bool_str( single_threaded ) <<  "\n" <<
-        " --scoring_strategy        " << scoring_strategy <<  "\n" <<
-        " --score_filtering         " << bool_str( score_filtering ) <<  "\n" <<
-        " --id_name_map             " << id_name_map_fname << "\n" <<
-        " --score_tie_threshold     " << score_tie_threshold << "\n" <<
-        " --score_overlap_threshold " << score_overlap_threshold << "\n" <<
-        " --output                  " << output_fname <<  "\n"
-        " --outfile_suffix          " << outfile_suffix <<  "\n"
-        " --scores_per_round        " << orig_scores_dname <<  "\n"
-        " --peptide_assignment_map  " << species_peptides_out <<  "\n"
-        " --mapfile_suffix          " << map_suffix <<  "\n"
-        " --enriched_file_ending    " << enriched_file_ending << "\n" <<
-        " --logfile                 " << logfile << "\n"
-        ;
-
-    str_stream << "\n";
-
+    str_stream
+        << "--linked                  " << linked_fname << "\n"
+        << "--threshold               " << threshold << "\n"
+        << "--enriched                " << enriched_fname << "\n"
+        << "--single_threaded         " << bool_str( single_threaded ) << "\n"
+        << "--scoring_strategy        " << scoring_strategy << "\n"
+        << "--score_filtering         " << bool_str( score_filtering ) << "\n"
+        << "--id_name_map             " << id_name_map_fname << "\n"
+        << "--score_tie_threshold     " << score_tie_threshold << "\n"
+        << "--score_overlap_threshold " << score_overlap_threshold << "\n"
+        << "--output                  " << output_fname << "\n"
+        << "--outfile_suffix          " << outfile_suffix << "\n"
+        << "--scores_per_round        " << orig_scores_dname << "\n"
+        << "--peptide_assignment_map  " << species_peptides_out << "\n"
+        << "--mapfile_suffix          " << map_suffix << "\n"
+        << "--enriched_file_ending    " << enriched_file_ending << "\n"
+        << "--logfile                 " << logfile << "\n\n";
 
     return str_stream.str();
 }
