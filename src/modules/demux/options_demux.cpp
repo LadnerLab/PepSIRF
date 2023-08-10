2d0f21cf3215e6425a0785a150897e548241c57b
Num0Programmer <asajh4@gmail.com>
2023-08-10 10:48:43 -0700

    Syncronized options_demux.cpp and options_deconv.cpp with the other modules' options class files.

--- src/modules/demux/options_demux.cpp
+++ src/modules/demux/options_demux.cpp
@@ -10,30 +10,30 @@
 std::string options_demux::get_arguments()
 {
     std::ostringstream str_stream;
-    str_stream << "--input_r1              " << input_r1_fname << "\n" <<
-                  " --input_r2              " << input_r2_fname << "\n" <<
-                  " --output                " << output_fname << "\n" <<
-                  " --aa_counts             " << aggregate_fname << "\n" <<
-                  " --translate_aggregates  " << std::boolalpha << translation_aggregation  << "\n" <<
-                  " --index                 " << index_fname << "\n" <<
-                  " --fif                   " << flexible_idx_fname << "\n" <<
-                  " --samplelist            " << samplelist_fname << "\n" <<
-                  " --sname                 " << samplename << "\n" <<
-                  " --sindex                " << indexes << "\n" <<
-                  " --library               " << library_fname << "\n" <<
-                  " --read_per_loop         " << read_per_loop << "\n" <<
-                  " --num_threads           " << num_threads << "\n" <<
-                  " --include_toggle        " << pos_toggle << "\n" <<
-                  " --seq                   " << tup_to_string( seq_data ) << "\n" <<
-                  " --index1                " << tup_to_string( index1_data ) << "\n" <<
-                  " --index2                " << tup_to_string( index2_data ) << "\n" <<
-                  " --phred_base            " << phred_base << "\n" <<
-                  " --phred_min_score       " << min_phred_score << "\n" <<
-                  " --concatemer            " << concatemer << "\n" <<
-                  " --diagnostic_info       " << diagnostic_fname << "\n" <<
-                  " --fastq_output          " << fastq_out << "\n" <<
-                  " --logfile               " << logfile << "\n"
-                  "\n";
+    str_stream
+        << "--input_r1              " << input_r1_fname << "\n"
+        << "--input_r2              " << input_r2_fname << "\n"
+        << "--output                " << output_fname << "\n"
+        << "--aa_counts             " << aggregate_fname << "\n"
+        << "--translate_aggregates  " << std::boolalpha << translation_aggregation << "\n"
+        << "--index                 " << index_fname << "\n"
+        << "--fif                   " << flexible_idx_fname << "\n"
+        << "--samplelist            " << samplelist_fname << "\n"
+        << "--sname                 " << samplename << "\n"
+        << "--sindex                " << indexes << "\n"
+        << "--library               " << library_fname << "\n"
+        << "--read_per_loop         " << read_per_loop << "\n"
+		<< "--num_threads           " << num_threads << "\n"
+        << "--include_toggle        " << pos_toggle << "\n"
+		<< "--seq                   " << tup_to_string( seq_data ) << "\n"
+		<< "--index1                " << tup_to_string( index1_data ) << "\n"
+		<< "--index2                " << tup_to_string( index2_data ) << "\n"
+		<< "--phred_base            " << phred_base << "\n"
+		<< "--phred_min_score       " << min_phred_score << "\n"
+		<< "--concatemer            " << concatemer << "\n"
+		<< "--diagnostic_info       " << diagnostic_fname << "\n"
+		<< "--fastq_output          " << fastq_out << "\n"
+		<< "--logfile               " << logfile << "\n\n";
 
     return str_stream.str();
 }
